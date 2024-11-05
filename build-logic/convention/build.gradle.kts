fcd57e4bd2a8cd250beef43eff46671f352d4224
HaeTi <t9h9@naver.com>
2024-11-05 21:44:03 +0900

    init/#1 : indent 조정

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,51 +1,51 @@
 plugins {
-    `kotlin-dsl`
+	`kotlin-dsl`
 }
 
 group = "com.boostcamp.mapisode.convention"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
-    compileOnly(libs.android.gradlePlugin)
-    compileOnly(libs.kotlin.gradlePlugin)
-    compileOnly(libs.compose.compiler.gradlePlugin)
-    compileOnly(libs.ksp.gradlePlugin)
+	compileOnly(libs.android.gradlePlugin)
+	compileOnly(libs.kotlin.gradlePlugin)
+	compileOnly(libs.compose.compiler.gradlePlugin)
+	compileOnly(libs.ksp.gradlePlugin)
 }
 
 gradlePlugin {
-    plugins {
-        register("androidApplication") {
-            id = "mapisode.android.application"
-            implementationClass = "AndroidApplicationPlugin"
-        }
+	plugins {
+		register("androidApplication") {
+			id = "mapisode.android.application"
+			implementationClass = "AndroidApplicationPlugin"
+		}
 
-        register("androidLibrary") {
-            id = "mapisode.android.library"
-            implementationClass = "AndroidLibraryPlugin"
-        }
+		register("androidLibrary") {
+			id = "mapisode.android.library"
+			implementationClass = "AndroidLibraryPlugin"
+		}
 
-        register("androidCompose") {
-            id = "mapisode.android.compose"
-            implementationClass = "AndroidComposePlugin"
-        }
+		register("androidCompose") {
+			id = "mapisode.android.compose"
+			implementationClass = "AndroidComposePlugin"
+		}
 
-        register("androidHilt") {
-            id = "mapisode.android.hilt"
-            implementationClass = "HiltPlugin"
-        }
+		register("androidHilt") {
+			id = "mapisode.android.hilt"
+			implementationClass = "HiltPlugin"
+		}
 
-        register("feature") {
-            id = "mapisode.feature"
-            implementationClass = "MapisodeFeaturePlugin"
-        }
+		register("feature") {
+			id = "mapisode.feature"
+			implementationClass = "MapisodeFeaturePlugin"
+		}
 
-        register("data") {
-            id = "mapisode.data"
-            implementationClass = "MapisodeDataPlugin"
-        }
-    }
+		register("data") {
+			id = "mapisode.data"
+			implementationClass = "MapisodeDataPlugin"
+		}
+	}
 }
