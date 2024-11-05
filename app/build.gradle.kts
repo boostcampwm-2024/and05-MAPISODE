5b9b450fd96ba230b94213418cf0831448fe8a2c
HaeTi <t9h9@naver.com>
2024-11-05 21:28:42 +0900

    init/#1 : ktlint 플러그인 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,54 +1,53 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.compose.compiler)
+	alias(libs.plugins.android.application)
+	alias(libs.plugins.kotlin.android)
+	alias(libs.plugins.compose.compiler)
 }
 
 android {
-    namespace = "com.boostcamp.mapisode"
-    compileSdk = 34
+	namespace = "com.boostcamp.mapisode"
+	compileSdk = 34
 
-    defaultConfig {
-        applicationId = "com.boostcamp.mapisode"
-        minSdk = 23
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+	defaultConfig {
+		applicationId = "com.boostcamp.mapisode"
+		minSdk = 23
+		targetSdk = 34
+		versionCode = 1
+		versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		vectorDrawables {
+			useSupportLibrary = true
+		}
+	}
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
+	}
+	kotlinOptions {
+		jvmTarget = "17"
+	}
+	buildFeatures {
+		compose = true
+	}
+	packaging {
+		resources {
+			excludes += "/META-INF/{AL2.0,LGPL2.1}"
+		}
+	}
 }
 
 dependencies {
-    implementation(libs.bundles.compose)
-    implementation(libs.bundles.androidx.core)
-
+	implementation(libs.bundles.compose)
+	implementation(libs.bundles.androidx.core)
 }
