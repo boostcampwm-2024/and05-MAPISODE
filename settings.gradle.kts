d4b797865d14eb42e58f3e17e54aa0fa17fa46d2
HaeTi <t9h9@naver.com>
2024-11-05 22:00:11 +0900

    init/#1 : trailing comma 허용

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,24 @@
 pluginManagement {
-    includeBuild("build-logic")
+	includeBuild("build-logic")
 
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-    }
+	repositories {
+		google {
+			content {
+				includeGroupByRegex("com\\.android.*")
+				includeGroupByRegex("com\\.google.*")
+				includeGroupByRegex("androidx.*")
+			}
+		}
+		mavenCentral()
+		gradlePluginPortal()
+	}
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+	repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+	repositories {
+		google()
+		mavenCentral()
+	}
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
