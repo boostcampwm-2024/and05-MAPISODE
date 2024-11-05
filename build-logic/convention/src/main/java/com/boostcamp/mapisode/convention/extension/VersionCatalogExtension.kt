8366b0a51bf3c80343257938bda2f1b1c08b6500
HaeTi <t9h9@naver.com>
2024-11-05 18:42:39 +0900

    init/#1 : simple expression 반영

--- build-logic/convention/src/main/java/com/boostcamp/mapisode/convention/extension/VersionCatalogExtension.kt
+++ build-logic/convention/src/main/java/com/boostcamp/mapisode/convention/extension/VersionCatalogExtension.kt
@@ -6,20 +6,17 @@ import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionConstraint
 import org.gradle.api.provider.Provider
 
-fun VersionCatalog.getBundle(bundleName: String): Provider<ExternalModuleDependencyBundle> {
-    return findBundle(bundleName).orElseThrow {
+fun VersionCatalog.getBundle(bundleName: String): Provider<ExternalModuleDependencyBundle> =
+    findBundle(bundleName).orElseThrow {
         NoSuchElementException("Bundle with name $bundleName not found in the catalog")
     }
-}
 
-fun VersionCatalog.getLibrary(libraryName: String): Provider<MinimalExternalModuleDependency> {
-    return findLibrary(libraryName).orElseThrow {
+fun VersionCatalog.getLibrary(libraryName: String): Provider<MinimalExternalModuleDependency> =
+    findLibrary(libraryName).orElseThrow {
         NoSuchElementException("Library with name $libraryName not found in the catalog")
     }
-}
 
-fun VersionCatalog.getVersion(versionName: String): VersionConstraint {
-    return findVersion(versionName).orElseThrow {
+fun VersionCatalog.getVersion(versionName: String): VersionConstraint =
+    findVersion(versionName).orElseThrow {
         NoSuchElementException("Version with name $versionName not found in the catalog")
     }
-}
