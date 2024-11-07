aeae892f0fe03dcac860061b3c8f02e7bb069019
HaeTi <t9h9@naver.com>
2024-11-07 16:49:41 +0900

    init/#5 : core:navigation 모듈 구현

---
+++ core/navigation/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+	alias(libs.plugins.mapisode.android.library)
+	alias(libs.plugins.mapisode.android.compose)
+	alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+	namespace = "com.boostcamp.mapisode.navigation"
+}
+
+dependencies {
+	implementation(libs.kotlinx.serialization.json)
+}
