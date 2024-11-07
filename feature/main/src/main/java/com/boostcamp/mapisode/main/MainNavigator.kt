cf546f070d1654df03268c2989dcdcbba13a78a0
HaeTi <t9h9@naver.com>
2024-11-08 00:33:06 +0900

    init/#5 : Main Navigation 구현

---
+++ feature/main/src/main/java/com/boostcamp/mapisode/main/MainNavigator.kt
@@ -0,0 +1,70 @@
+package com.boostcamp.mapisode.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.boostcamp.mapisode.navigation.MainRoute
+
+internal class MainNavigator(
+	val navController: NavHostController,
+) {
+	private val currentDestination: NavDestination?
+		@Composable get() = navController
+			.currentBackStackEntryAsState().value?.destination
+
+	val startDestination = MainNavTab.HOME.route
+
+	val currentTab: MainNavTab?
+		@Composable get() = MainNavTab.find { tab ->
+			currentDestination?.hasRoute(tab::class) == true
+		}
+
+	fun navigate(tab: MainNavTab) {
+		val navOptions = navOptions {
+			popUpTo(navController.graph.findStartDestination().id) {
+				saveState = true
+			}
+			launchSingleTop = true
+			restoreState = true
+		}
+
+		when (tab) {
+			MainNavTab.HOME -> navController.navigate(MainNavTab.HOME.route, navOptions)
+			MainNavTab.EPISODE -> navController.navigate(MainNavTab.EPISODE.route, navOptions)
+			MainNavTab.GROUP -> navController.navigate(MainNavTab.GROUP.route, navOptions)
+			MainNavTab.MYPAGE -> navController.navigate(MainNavTab.MYPAGE.route, navOptions)
+		}
+	}
+
+	private fun popBackStack() {
+		navController.popBackStack()
+	}
+
+	fun popBackStackIfNotHome() {
+		if (!isSameCurrentDestination<MainRoute.Home>()) {
+			popBackStack()
+		}
+	}
+
+	private inline fun <reified T : MainRoute> isSameCurrentDestination(): Boolean {
+		return navController.currentDestination?.hasRoute<T>() == true
+	}
+
+	@Composable
+	fun shouldShowBottomBar() = MainNavTab.contains {
+		currentDestination?.hasRoute(it::class) == true
+	}
+}
+
+@Composable
+internal fun rememberMainNavigator(
+	navController: NavHostController = rememberNavController(),
+): MainNavigator = remember(navController) {
+	MainNavigator(navController)
+}
---
+++ feature/main/src/main/java/com/boostcamp/mapisode/main/component/MainNavHost.kt
@@ -0,0 +1,37 @@
+package com.boostcamp.mapisode.main.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import com.boostcamp.mapisode.episode.navigation.addEpisodeNavGraph
+import com.boostcamp.mapisode.home.navigation.addHomeNavGraph
+import com.boostcamp.mapisode.main.MainNavigator
+import com.boostcamp.mapisode.mygroup.navigation.addGroupNavGraph
+import com.boostcamp.mapisode.mypage.navigation.addMyPageNavGraph
+
+@Composable
+internal fun MainNavHost(
+	modifier: Modifier = Modifier,
+	navigator: MainNavigator,
+) {
+	Box(
+		modifier = modifier
+			.fillMaxSize()
+			.background(Color.White),
+	) {
+		NavHost(
+			navController = navigator.navController,
+			startDestination = navigator.startDestination,
+		) {
+			addHomeNavGraph()
+			addEpisodeNavGraph()
+			addGroupNavGraph()
+			addMyPageNavGraph()
+		}
+	}
+
+}
