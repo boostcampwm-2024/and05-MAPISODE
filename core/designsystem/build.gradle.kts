411c1e95b8233475c45250d905417ff1b6c30cca
TaewoongR <iwoong98@kau.kr>
2024-11-10 06:23:31 +0900

    init/#12 : design system 모듈 테스트 gradle 세팅

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
 	alias(libs.plugins.mapisode.android.compose)
+	alias(libs.plugins.compose.compiler)
 }
 
 android {
 	namespace = "com.boostcamp.mapisode.designsystem"
+	defaultConfig {
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
 }
 
 dependencies {
 	implementation(projects.core.ui)
 	implementation(projects.core.model)
+	androidTestImplementation(libs.bundles.test)
 }
