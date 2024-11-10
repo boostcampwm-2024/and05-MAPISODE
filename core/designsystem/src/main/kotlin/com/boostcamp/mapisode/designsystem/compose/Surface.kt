c095dc47541bebc7b534ae3fc087b805d22fa687
TaewoongR <iwoong98@kau.kr>
2024-11-11 00:52:33 +0900

    feat/#14 : Custom Surface 생성

---
+++ core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/Surface.kt
@@ -0,0 +1,97 @@
+package com.boostcamp.mapisode.designsystem.compose
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.Dp
+import com.boostcamp.mapisode.designsystem.theme.LocalMapisodeContentColor
+import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
+
+@Composable
+fun Surface(
+	modifier: Modifier = Modifier,
+	rounding: Dp? = null,
+	shape: Shape = rounding?.let { RoundedCornerShape(it) } ?: RectangleShape,
+	color: Color = MapisodeTheme.colorScheme.background,
+	contentColor: Color = LocalMapisodeContentColor.current,
+	border: BorderStroke? = null,
+	content: @Composable () -> Unit,
+) {
+	CompositionLocalProvider(
+		LocalMapisodeContentColor provides contentColor,
+	) {
+		Box(
+			modifier = modifier
+                .surface(
+                    shape = shape,
+                    backgroundColor = color,
+                    border = border,
+                )
+                .semantics { isTraversalGroup = true }
+                .pointerInput(Unit) {},
+			propagateMinConstraints = true,
+		) {
+			content()
+		}
+	}
+}
+
+@Composable
+fun Surface(
+	onClick: () -> Unit,
+	modifier: Modifier = Modifier,
+	enabled: Boolean = true,
+	rounding: Dp? = null,
+	shape: Shape = rounding?.let { RoundedCornerShape(it) } ?: RectangleShape,
+	color: Color = MapisodeTheme.colorScheme.background,
+	contentColor: Color = LocalMapisodeContentColor.current,
+	border: BorderStroke? = null,
+	interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+	content: @Composable () -> Unit,
+) {
+	CompositionLocalProvider(
+		LocalMapisodeContentColor provides contentColor,
+	) {
+		Box(
+			modifier = modifier
+                .surface(
+                    shape = shape,
+                    backgroundColor = color,
+                    border = border,
+                )
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null,
+                    enabled = enabled,
+                    onClick = onClick,
+                ),
+			propagateMinConstraints = true,
+		) {
+			content()
+		}
+	}
+}
+
+private fun Modifier.surface(
+	shape: Shape,
+	backgroundColor: Color,
+	border: BorderStroke?,
+): Modifier = this
+    .then(if (border != null) Modifier.border(border, shape) else Modifier)
+    .background(color = backgroundColor, shape = shape)
+    .clip(shape)
