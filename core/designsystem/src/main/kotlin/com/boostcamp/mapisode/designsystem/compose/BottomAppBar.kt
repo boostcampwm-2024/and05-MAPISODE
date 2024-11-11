e40f1e238ebc1495cb8f186378a23f9d96833e3e
TaewoongR <iwoong98@kau.kr>
2024-11-11 17:13:51 +0900

    feat/#14 : Custom BottomAppBar 컴포넌트 생성

---
+++ core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/BottomAppBar.kt
@@ -0,0 +1,52 @@
+package com.boostcamp.mapisode.designsystem.compose
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
+
+@Composable
+fun BottomBar(
+	modifier: Modifier = Modifier,
+	visible: Boolean = true,
+	backgroundColor: Color = MapisodeTheme.colorScheme.background,
+	contentColor: Color = MapisodeTheme.colorScheme.secondaryText,
+	content: @Composable RowScope.() -> Unit,
+) {
+	Surface(
+		color = backgroundColor,
+		contentColor = contentColor,
+		modifier = modifier
+            .fillMaxWidth()
+            .height(68.dp),
+	) {
+		Column {
+			MapisodeDivider(thickness = Thickness.Thin)
+			AnimatedVisibility(
+				visible = visible,
+				enter = fadeIn(),
+				exit = fadeOut(),
+			) {
+				Row(
+					Modifier
+						.selectableGroup(),
+					horizontalArrangement = Arrangement.SpaceBetween,
+					verticalAlignment = Alignment.CenterVertically,
+					content = content,
+				)
+			}
+		}
+	}
+}
