b4005de366f36ec23edd700a3aaea770e9009dbb
TaewoongR <iwoong98@kau.kr>
2024-11-14 02:56:47 +0900

    chore/#23 : NavHost의 Box background 삭제
    - insets 설정으로 인해 background 설정과 맞물려 status bar의 content가 안보임

--- feature/main/src/main/java/com/boostcamp/mapisode/main/component/MainNavHost.kt
+++ feature/main/src/main/java/com/boostcamp/mapisode/main/component/MainNavHost.kt
@@ -1,12 +1,10 @@
 package com.boostcamp.mapisode.main.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import com.boostcamp.mapisode.episode.navigation.addEpisodeNavGraph
 import com.boostcamp.mapisode.home.navigation.addHomeNavGraph
@@ -22,7 +20,6 @@ internal fun MainNavHost(
 	Box(
 		modifier = modifier
 			.fillMaxSize()
-			.background(Color.White)
 			.statusBarsPadding(),
 	) {
 		NavHost(
