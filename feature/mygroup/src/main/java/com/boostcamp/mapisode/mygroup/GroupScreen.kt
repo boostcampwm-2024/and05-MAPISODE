2aff14522e04d6351b6e1e47c6c43ff99da5aef0
TaewoongR <iwoong98@kau.kr>
2024-11-14 01:37:46 +0900

    chore/#23 : status bar 패딩 적용 화면 구성
    - 스크린 최상단을 offset으로 한 Scaffold 적용
    - Scaffold 높이의 하단이 네비게이션바 위에까지 차지하는지 확인

--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/GroupScreen.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/GroupScreen.kt
@@ -1,12 +1,18 @@
 package com.boostcamp.mapisode.mygroup
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.boostcamp.mapisode.designsystem.compose.MapisodeScaffold
+import com.boostcamp.mapisode.designsystem.compose.MapisodeText
+import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
 
 @Composable
 internal fun GroupRoute() {
@@ -15,10 +21,19 @@ internal fun GroupRoute() {
 
 @Composable
 private fun GroupScreen() {
-	Box(
-		modifier = Modifier.fillMaxSize(),
-		contentAlignment = Alignment.Center,
+	MapisodeScaffold(
+		modifier = Modifier
+			.fillMaxSize()
+			.offset(y = -WindowInsets.statusBars.asPaddingValues().calculateTopPadding()),
 	) {
-		Text(text = "GroupScreen", style = MaterialTheme.typography.displayMedium)
+		Box(
+			modifier = Modifier.fillMaxSize().padding(it),
+			contentAlignment = Alignment.Center,
+		) {
+			MapisodeText(
+				text = "Group",
+				style = MapisodeTheme.typography.displayMedium,
+			)
+		}
 	}
 }
