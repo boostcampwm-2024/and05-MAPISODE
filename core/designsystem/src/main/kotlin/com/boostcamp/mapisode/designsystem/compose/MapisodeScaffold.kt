5266d2b1e4c8ee1cdebdd3648d37f0ca2b94419e
TaewoongR <iwoong98@kau.kr>
2024-11-14 01:31:20 +0900

    chore/#23 : status bar 패딩 유무에 따른 height 설정
    - map 화면인 경우 status bar 위에 표시할 예정이며, 다른 화면은 status bar 패딩을 두고 배치 예정.

--- core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/MapisodeScaffold.kt
+++ core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/MapisodeScaffold.kt
@@ -1,10 +1,19 @@
 package com.boostcamp.mapisode.designsystem.compose
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.layout.onGloballyPositioned
 import com.boostcamp.mapisode.designsystem.compose.toast.ToastHost
 import com.boostcamp.mapisode.designsystem.compose.toast.ToastHostState
 import com.boostcamp.mapisode.designsystem.compose.toast.rememberToastHostState
@@ -24,8 +33,19 @@ fun MapisodeScaffold(
 	contentColor: Color = LocalMapisodeContentColor.current,
 	content: @Composable (PaddingValues) -> Unit,
 ) {
-	Surface(modifier = modifier, color = backgroundColor, contentColor = contentColor) {
+	var parentOffsetY by remember { mutableFloatStateOf(0f) }
+
+	Surface(
+		modifier = modifier
+			.onGloballyPositioned { layoutCoordinates ->
+				val offset = layoutCoordinates.localToWindow(Offset.Zero)
+				parentOffsetY = offset.y
+			},
+		color = backgroundColor,
+		contentColor = contentColor,
+	) {
 		ScaffoldLayout(
+			parentOffsetY = parentOffsetY,
 			topBar = topBar,
 			bottomBar = bottomBar,
 			toast = { toastHost(toastHostState) },
@@ -36,11 +56,15 @@ fun MapisodeScaffold(
 
 @Composable
 private fun ScaffoldLayout(
+	parentOffsetY: Float,
 	topBar: @Composable () -> Unit,
 	bottomBar: @Composable () -> Unit,
 	toast: @Composable () -> Unit,
 	content: @Composable (PaddingValues) -> Unit,
 ) {
+	val isStatusBarsPaddingEnabled = parentOffsetY == 0f
+	val statusBarPadding = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+
 	SubcomposeLayout { constraints ->
 		val layoutWidth = constraints.maxWidth
 		val layoutHeight = constraints.maxHeight
@@ -61,9 +85,10 @@ private fun ScaffoldLayout(
 
 			val toastHeight = toastPlaceables.maxByOrNull { it.height }?.height ?: 0
 
-			val bottomBarPlaceables = subcompose(ScaffoldLayoutContent.BottomBar, bottomBar).map {
-				it.measure(looseConstraints)
-			}
+			val bottomBarPlaceables =
+				subcompose(ScaffoldLayoutContent.BottomBar, bottomBar).map {
+					it.measure(looseConstraints)
+				}
 
 			val bottomBarHeight = bottomBarPlaceables.maxByOrNull { it.height }?.height ?: 0
 
@@ -73,7 +98,10 @@ private fun ScaffoldLayout(
 				0
 			}
 
-			val bodyContentHeight = layoutHeight - topBarHeight
+			val bodyContentHeight =
+				layoutHeight - topBarHeight +
+					if (isStatusBarsPaddingEnabled) statusBarPadding.toPx()
+						.toInt() else 0
 
 			val bodyContentPlaceables = subcompose(ScaffoldLayoutContent.MainContent) {
 				val innerPadding = PaddingValues(bottom = bottomBarHeight.toDp())
