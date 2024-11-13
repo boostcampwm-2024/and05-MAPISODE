d24feb080e52c396e3441a5a0da330d64b324da0
HaeTi <t9h9@naver.com>
2024-11-14 01:56:53 +0900

    feat/#22 : 현재 위치 타켓 기능이 추가된 지도 구현

--- feature/home/src/main/java/com/boostcamp/mapisode/home/HomeState.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/HomeState.kt
@@ -1,7 +1,17 @@
 package com.boostcamp.mapisode.home
 
+import com.boostcamp.mapisode.home.common.HomeConstant.DEFAULT_ZOOM
 import com.boostcamp.mapisode.ui.base.UiState
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
 
 data class HomeState(
-	val count: Int = 0,
+	val cameraPosition: CameraPosition = CameraPosition(
+		LatLng(
+			37.38026976485322,
+			127.11537099437301,
+		),
+		DEFAULT_ZOOM,
+	),
+	val isInitialLocationSet: Boolean = false,
 ) : UiState
---
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/common/Const.kt
@@ -0,0 +1,5 @@
+package com.boostcamp.mapisode.home.common
+
+object HomeConstant {
+	const val DEFAULT_ZOOM = 16.0
+}
