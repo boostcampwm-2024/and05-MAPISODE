555402c9b6b0af07d1351b187c474721e1bccd0a
TaewoongR <iwoong98@kau.kr>
2024-11-14 06:46:15 +0900

    chore/#23 : Custom Scaffold 대규모 수정
    - 최대한 Material과 유사하게 변형
    - Scaffold 외부에서 함수 인자로 insets 존재여부를 결정한 뒤에 내부에서 insets에 대한 padding 부여
    - Scaffold 내부에서만 inset 관련 로직 실행

--- core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/MapisodeScaffold.kt
+++ core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/MapisodeScaffold.kt
@@ -3,17 +3,16 @@ package com.boostcamp.mapisode.designsystem.compose
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.SubcomposeLayout
-import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.offset
+import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.util.fastMap
+import androidx.compose.ui.util.fastMaxBy
 import com.boostcamp.mapisode.designsystem.compose.toast.ToastHost
 import com.boostcamp.mapisode.designsystem.compose.toast.ToastHostState
 import com.boostcamp.mapisode.designsystem.compose.toast.rememberToastHostState
@@ -25,6 +24,7 @@ private enum class ScaffoldLayoutContent { TopBar, MainContent, Snackbar, Bottom
 @Composable
 fun MapisodeScaffold(
 	modifier: Modifier = Modifier,
+	isStatusBarPaddingExist: Boolean = false,
 	toastHostState: ToastHostState = rememberToastHostState(),
 	topBar: @Composable () -> Unit = {},
 	bottomBar: @Composable () -> Unit = {},
@@ -33,19 +33,13 @@ fun MapisodeScaffold(
 	contentColor: Color = LocalMapisodeContentColor.current,
 	content: @Composable (PaddingValues) -> Unit,
 ) {
-	var parentOffsetY by remember { mutableFloatStateOf(0f) }
-
 	Surface(
-		modifier = modifier
-			.onGloballyPositioned { layoutCoordinates ->
-				val offset = layoutCoordinates.localToWindow(Offset.Zero)
-				parentOffsetY = offset.y
-			},
+		modifier = modifier,
 		color = backgroundColor,
 		contentColor = contentColor,
 	) {
 		ScaffoldLayout(
-			parentOffsetY = parentOffsetY,
+			isStatusBarPaddingExist = isStatusBarPaddingExist,
 			topBar = topBar,
 			bottomBar = bottomBar,
 			toast = { toastHost(toastHostState) },
@@ -56,14 +50,15 @@ fun MapisodeScaffold(
 
 @Composable
 private fun ScaffoldLayout(
-	parentOffsetY: Float,
+	isStatusBarPaddingExist: Boolean,
 	topBar: @Composable () -> Unit,
 	bottomBar: @Composable () -> Unit,
 	toast: @Composable () -> Unit,
 	content: @Composable (PaddingValues) -> Unit,
 ) {
-	val isStatusBarsPaddingEnabled = parentOffsetY == 0f
-	val statusBarPadding = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+	val topPadding = if (isStatusBarPaddingExist) WindowInsets.systemBars.asPaddingValues()
+		.calculateTopPadding() else 0.dp
+	val systemBar = WindowInsets(top = topPadding, bottom = 0.dp)
 
 	SubcomposeLayout { constraints ->
 		val layoutWidth = constraints.maxWidth
@@ -71,58 +66,73 @@ private fun ScaffoldLayout(
 
 		val looseConstraints = constraints.copy(minWidth = 0, minHeight = 0)
 
-		layout(layoutWidth, layoutHeight) {
+		val topBarPlaceables = subcompose(ScaffoldLayoutContent.TopBar, topBar).map {
+			it.measure(looseConstraints)
+		}
+		val topBarHeight = topBarPlaceables.fastMaxBy { it.height }?.height ?: 0
 
-			val topBarPlaceables = subcompose(ScaffoldLayoutContent.TopBar, topBar).map {
-				it.measure(looseConstraints)
+		val toastPlaceables =
+			subcompose(ScaffoldLayoutContent.Snackbar, toast).fastMap {
+				val bottomInset = systemBar.getBottom(this@SubcomposeLayout)
+				it.measure(looseConstraints.offset(vertical = -bottomInset))
 			}
+		val toastHeight = toastPlaceables.fastMaxBy { it.height }?.height ?: 0
+		val toastWidth = toastPlaceables.fastMaxBy { it.width }?.width ?: 0
 
-			val topBarHeight = topBarPlaceables.maxByOrNull { it.height }?.height ?: 0
+		val bottomBarPlaceables =
+			subcompose(ScaffoldLayoutContent.BottomBar) { bottomBar() }
+				.fastMap { it.measure(looseConstraints) }
 
-			val toastPlaceables = subcompose(ScaffoldLayoutContent.Snackbar, toast).map {
-				it.measure(looseConstraints)
-			}
+		val bottomBarHeight = bottomBarPlaceables.fastMaxBy { it.height }?.height
 
-			val toastHeight = toastPlaceables.maxByOrNull { it.height }?.height ?: 0
-
-			val bottomBarPlaceables =
-				subcompose(ScaffoldLayoutContent.BottomBar, bottomBar).map {
-					it.measure(looseConstraints)
-				}
-
-			val bottomBarHeight = bottomBarPlaceables.maxByOrNull { it.height }?.height ?: 0
-
-			val snackbarOffsetFromBottom = if (toastHeight != 0) {
-				toastHeight + bottomBarHeight + 10
+		val toastOffsetFromBottom =
+			if (toastHeight != 0) {
+				toastHeight +
+					(bottomBarHeight ?: systemBar.getBottom(this@SubcomposeLayout))
 			} else {
 				0
 			}
 
-			val bodyContentHeight =
-				layoutHeight - topBarHeight +
-					if (isStatusBarsPaddingEnabled) {
-						statusBarPadding.toPx().toInt()
-					} else {
-						0
-					}
-
-			val bodyContentPlaceables = subcompose(ScaffoldLayoutContent.MainContent) {
-				val innerPadding = PaddingValues(bottom = bottomBarHeight.toDp())
+		val bodyContentPlaceables =
+			subcompose(ScaffoldLayoutContent.MainContent) {
+				val insets = systemBar.asPaddingValues(this@SubcomposeLayout)
+				val innerPadding =
+					PaddingValues(
+						top = if (topBarPlaceables.isEmpty()) {
+							insets.calculateTopPadding()
+						} else {
+							topBarHeight.toDp()
+						},
+						bottom =
+						if (bottomBarPlaceables.isEmpty() || bottomBarHeight == null) {
+							insets.calculateBottomPadding()
+						} else {
+							bottomBarHeight.toDp()
+						},
+					)
 				content(innerPadding)
-			}.map { it.measure(looseConstraints.copy(maxHeight = bodyContentHeight)) }
+			}
+				.fastMap {
+					val statusBarHeight = systemBar.getTop(this@SubcomposeLayout)
+					val adjustedConstraints = looseConstraints.copy(
+						maxHeight = looseConstraints.maxHeight - statusBarHeight,
+					)
+					it.measure(adjustedConstraints)
+				}
 
-			bodyContentPlaceables.forEach {
-				it.place(0, topBarHeight)
+		layout(layoutWidth, layoutHeight) {
+			val topBarOffset = systemBar.getTop(this@SubcomposeLayout)
+
+			bodyContentPlaceables.fastForEach { it.place(0, topBarOffset) }
+			topBarPlaceables.fastForEach { it.place(0, topBarOffset) }
+			toastPlaceables.fastForEach {
+				it.place(
+					(layoutWidth - toastWidth) / 2 +
+						systemBar.getLeft(this@SubcomposeLayout, layoutDirection),
+					layoutHeight - toastOffsetFromBottom,
+				)
 			}
-			topBarPlaceables.forEach {
-				it.place(0, 0)
-			}
-			toastPlaceables.forEach {
-				it.place(0, layoutHeight - snackbarOffsetFromBottom)
-			}
-			bottomBarPlaceables.forEach {
-				it.place(0, layoutHeight - bottomBarHeight)
-			}
+			bottomBarPlaceables.fastForEach { it.place(0, layoutHeight - (bottomBarHeight ?: 0)) }
 		}
 	}
 }
--- feature/main/src/main/java/com/boostcamp/mapisode/main/component/MainNavHost.kt
+++ feature/main/src/main/java/com/boostcamp/mapisode/main/component/MainNavHost.kt
@@ -2,7 +2,6 @@ package com.boostcamp.mapisode.main.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
@@ -18,9 +17,7 @@ internal fun MainNavHost(
 	navigator: MainNavigator,
 ) {
 	Box(
-		modifier = modifier
-			.fillMaxSize()
-			.statusBarsPadding(),
+		modifier = modifier.fillMaxSize(),
 	) {
 		NavHost(
 			navController = navigator.navController,
