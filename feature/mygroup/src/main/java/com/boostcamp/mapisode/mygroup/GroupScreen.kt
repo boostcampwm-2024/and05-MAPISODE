42cefc10d743963b783910ef5f323d9d59d3fbf9
TaewoongR <iwoong98@kau.kr>
2024-11-14 06:46:15 +0900

    chore/#23 : Custom Scaffold 대규모 수정
    - 최대한 Material과 유사하게 변형
    - Scaffold 외부에서 함수 인자로 insets 존재여부를 결정한 뒤에 내부에서 insets에 대한 padding 부여
    - Scaffold 내부에서만 inset 관련 로직 실행

--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/GroupScreen.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/GroupScreen.kt
@@ -1,17 +1,14 @@
 package com.boostcamp.mapisode.mygroup
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.boostcamp.mapisode.designsystem.compose.MapisodeScaffold
 import com.boostcamp.mapisode.designsystem.compose.MapisodeText
+import com.boostcamp.mapisode.designsystem.compose.topbar.TopAppBar
 import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
 
 @Composable
@@ -23,11 +20,18 @@ internal fun GroupRoute() {
 private fun GroupScreen() {
 	MapisodeScaffold(
 		modifier = Modifier
-			.fillMaxSize()
-			.offset(y = -WindowInsets.statusBars.asPaddingValues().calculateTopPadding()),
+			.fillMaxSize(),
+		isStatusBarPaddingExist = true,
+		topBar = {
+			TopAppBar(
+				title = "나의 그룹",
+			)
+		},
 	) {
 		Box(
-			modifier = Modifier.fillMaxSize().padding(it),
+			modifier = Modifier
+				.fillMaxSize()
+				.padding(it),
 			contentAlignment = Alignment.Center,
 		) {
 			MapisodeText(
