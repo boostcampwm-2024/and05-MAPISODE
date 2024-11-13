5774db6e5c8cd7f9ca39ad1a5f7261e9cb7e15a6
TaewoongR <iwoong98@kau.kr>
2024-11-14 01:27:29 +0900

    chore/#23 : status bar 패딩만 적용
    - 내부 Scaffold 에서 bottom navigation bar를 선언할 상황이 없음

--- feature/main/src/main/java/com/boostcamp/mapisode/main/component/MainNavHost.kt
+++ feature/main/src/main/java/com/boostcamp/mapisode/main/component/MainNavHost.kt
@@ -3,7 +3,7 @@ package com.boostcamp.mapisode.main.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,7 +23,7 @@ internal fun MainNavHost(
 		modifier = modifier
 			.fillMaxSize()
 			.background(Color.White)
-			.systemBarsPadding(),
+			.statusBarsPadding(),
 	) {
 		NavHost(
 			navController = navigator.navController,
