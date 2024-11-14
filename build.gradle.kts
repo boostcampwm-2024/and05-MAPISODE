900b1f80c09c5ccf49375788e98dfb7605b4575d
Ameri-Kano <yeokkim13@naver.com>
2024-11-14 20:17:28 +0900

    feat/#15 : google-service 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 	alias(libs.plugins.ksp) apply false
 	alias(libs.plugins.android.library) apply false
 	alias(libs.plugins.ktlint) apply false
+	id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 buildscript {
