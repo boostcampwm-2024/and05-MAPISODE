5b4072a12e562c1fbbce0127d3e926fb4b86fc7e
HaeTi <t9h9@naver.com>
2024-11-15 03:40:03 +0900

    feat/#27 : Chip UI 구현

---
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/common/ChipType.kt
@@ -0,0 +1,26 @@
+package com.boostcamp.mapisode.home.common
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
+import com.boostcamp.mapisode.home.R
+
+enum class ChipType(
+	@StringRes val textResId: Int,
+	@DrawableRes val iconResId: Int,
+) {
+	EATERY(R.string.home_chip_eat, R.drawable.ic_eat),
+	SEEING(R.string.home_chip_see, R.drawable.ic_see),
+	OTHER(R.string.home_chip_other, R.drawable.ic_other)
+}
+
+@Composable
+fun getChipIconTint(chipType: ChipType): Color {
+	return when (chipType) {
+		ChipType.EATERY -> MapisodeTheme.colorScheme.eatIconColor
+		ChipType.SEEING -> MapisodeTheme.colorScheme.seeIconColor
+		ChipType.OTHER -> MapisodeTheme.colorScheme.otherIconColor
+	}
+}
