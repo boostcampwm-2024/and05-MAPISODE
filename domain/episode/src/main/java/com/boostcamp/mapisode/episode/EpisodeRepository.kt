563fc30140dd3ea87121aca2102e268e50d4fc47
Ameri-Kano <yeokkim13@naver.com>
2024-11-15 15:57:45 +0900

    fix/#15 : DTO -> Model 로 이름 변경 및 EpisodeLatLng 추가

--- domain/episode/src/main/java/com/boostcamp/mapisode/episode/EpisodeRepository.kt
+++ domain/episode/src/main/java/com/boostcamp/mapisode/episode/EpisodeRepository.kt
@@ -1,19 +1,19 @@
 package com.boostcamp.mapisode.episode
 
 interface EpisodeRepository {
-	suspend fun getEpisodesByGroup(groupId: String): List<EpisodeDTO>
+	suspend fun getEpisodesByGroup(groupId: String): List<EpisodeModel>
 	suspend fun getEpisodesByGroupAndLocation(
 		groupId: String,
 		start: Pair<Double, Double>,
 		end: Pair<Double, Double>,
-	): List<EpisodeDTO>
+	): List<EpisodeModel>
 
 	suspend fun getEpisodesByGroupAndCategory(
 		groupId: String,
 		category: String,
-	): List<EpisodeDTO>
+	): List<EpisodeModel>
 
-	suspend fun getEpisodeById(episodeId: String): EpisodeDTO?
+	suspend fun getEpisodeById(episodeId: String): EpisodeModel?
 
-	suspend fun createEpisode(episodeDTO: EpisodeDTO): String
+	suspend fun createEpisode(episodeModel: EpisodeModel): String
 }
