f2fc06e265679c1a476af755739db7599ea1e2fc
TaewoongR <iwoong98@kau.kr>
2024-11-19 00:52:05 +0900

    feat/#48 : Custom ImageButton 테스트
    - 추가 화면 생성 및 ImageButton 적용
    - 모듈 레벨 gralde에 coil 추가 후 에뮬레이터 테스트

---
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/GroupEditScreen.kt
@@ -0,0 +1,82 @@
+package com.boostcamp.mapisode.mygroup
+
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import coil3.compose.AsyncImage
+import com.boostcamp.mapisode.designsystem.R
+import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
+import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
+import com.boostcamp.mapisode.designsystem.compose.MapisodeScaffold
+import com.boostcamp.mapisode.designsystem.compose.button.MapisodeImageButton
+import com.boostcamp.mapisode.designsystem.compose.topbar.TopAppBar
+
+@Composable
+fun GroupEditScreen() {
+	val focusManager = LocalFocusManager.current
+
+	MapisodeScaffold(
+		modifier = Modifier
+			.fillMaxSize()
+			.pointerInput(Unit) {
+				detectTapGestures(
+					onPress = {
+						focusManager.clearFocus()
+					},
+				)
+			},
+		isStatusBarPaddingExist = true,
+		topBar = {
+			TopAppBar(
+				title = "나의 그룹",
+				navigationIcon = {
+					MapisodeIconButton(
+						onClick = {
+						},
+					) {
+						MapisodeIcon(
+							id = R.drawable.ic_arrow_back_ios,
+						)
+					}
+				},
+			)
+		},
+	) {
+		var isClicked by remember { mutableStateOf(true) }
+		Column(
+			modifier = Modifier
+				.fillMaxSize()
+				.padding(it),
+			verticalArrangement = Arrangement.Center,
+			horizontalAlignment = Alignment.CenterHorizontally,
+		) {
+			MapisodeImageButton(
+				modifier = Modifier
+					.fillMaxWidth(0.8f)
+					.aspectRatio(1f),
+				onClick = { isClicked = !isClicked },
+				showImage = isClicked,
+				text = "이미지를 선택하세요",
+			) {
+				AsyncImage(
+					model = "https://m.media-amazon.com/images/I/61jyqnlyIaS.jpg",
+					contentDescription = "그룹 이미지",
+					modifier = Modifier.fillMaxSize(),
+				)
+			}
+		}
+	}
+}
