0043d9dae6246fdd15e9996f911c6f95de58a1c6
TaewoongR <iwoong98@kau.kr>
2024-11-19 00:52:05 +0900

    feat/#48 : Custom ImageButton 테스트
    - 추가 화면 생성 및 ImageButton 적용
    - 모듈 레벨 gralde에 coil 추가 후 에뮬레이터 테스트

--- feature/mygroup/build.gradle.kts
+++ feature/mygroup/build.gradle.kts
@@ -1,7 +1,13 @@
+import com.boostcamp.mapisode.convention.extension.implementation
+
 plugins {
 	alias(libs.plugins.mapisode.feature)
 }
 
 android {
 	namespace = "com.boostcamp.mapisode.mygroup"
+}
+
+dependencies {
+	implementation(libs.bundles.coil)
 }
