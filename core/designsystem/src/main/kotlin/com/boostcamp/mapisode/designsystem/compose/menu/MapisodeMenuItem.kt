34fe0f769cf7ba0411985f894368254cc982ee3c
TaewoongR <iwoong98@kau.kr>
2024-11-18 20:20:30 +0900

    fix/#49 : menuItem 리플 수정
    - 아래 드롭다운 메뉴아이템의 리플이 위에도 영향을 끼쳐 clip으로 아이템에만 리플이 뜨도록 해결

--- core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/menu/MapisodeMenuItem.kt
+++ core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/menu/MapisodeMenuItem.kt
@@ -11,8 +11,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import com.boostcamp.mapisode.designsystem.compose.Surface
+import com.boostcamp.mapisode.designsystem.compose.ripple.MapisodeRippleBIndication
 import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
 
 @Composable
@@ -20,17 +23,18 @@ fun MapisodeDropdownMenuItem(
 	onClick: () -> Unit,
 	modifier: Modifier = Modifier,
 	enabled: Boolean = true,
-	contentPadding: PaddingValues = MapisodeMenuDefaults.DropdownMenuItemContentPadding,
+	contentPadding: PaddingValues = MapisodeMenuItemDefaults.DropdownMenuItemContentPadding,
 	interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
 	content: @Composable RowScope.() -> Unit,
 ) {
 	Surface(
 		modifier = modifier
+			.clip(RectangleShape)
 			.clickable(
 				enabled = enabled,
 				onClick = onClick,
 				interactionSource = interactionSource,
-				indication = null,
+				indication = MapisodeRippleBIndication,
 			)
 			.fillMaxWidth(),
 		color = if (enabled) {
@@ -47,7 +51,7 @@ fun MapisodeDropdownMenuItem(
 	}
 }
 
-private object MapisodeMenuDefaults {
+private object MapisodeMenuItemDefaults {
 	val DropdownMenuItemContentPadding = PaddingValues(
 		horizontal = 16.dp,
 		vertical = 8.dp,
