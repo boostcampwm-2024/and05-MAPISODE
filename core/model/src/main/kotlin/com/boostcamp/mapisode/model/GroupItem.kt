af2b421d22d1514b1f4b6fe0e74d6adcd2d07f1d
HaeTi <t9h9@naver.com>
2024-11-19 21:54:39 +0900

    feat/#66 : 그룹 선택 BottomSheet UI 구현

---
+++ core/model/src/main/kotlin/com/boostcamp/mapisode/model/GroupItem.kt
@@ -0,0 +1,9 @@
+package com.boostcamp.mapisode.model
+
+data class GroupItem(
+	val name: String,
+	val imageUrl: String,
+	val description: String,
+	val createdAt: String,
+	val adminUser: String,
+)
