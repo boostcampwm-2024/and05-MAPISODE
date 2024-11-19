5897f2285b5ab9c20e43dd81645efe5b800d1e3d
TaewoongR <iwoong98@kau.kr>
2024-11-20 00:22:23 +0900

    feat/#65 : Custom Dialog 디자인 완성

--- core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/MapisodeDialog.kt
+++ core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/MapisodeDialog.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -21,7 +22,6 @@ import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
 
 @Composable
 fun MapisodeDialog(
-	modifier: Modifier = Modifier,
 	onResultRequest: (Boolean) -> Unit,
 	onDismissRequest: () -> Unit,
 	titleText: String,
@@ -41,17 +41,16 @@ fun MapisodeDialog(
 	) {
 		Surface(
 			color = MapisodeTheme.colorScheme.dialogBackground,
+			rounding = 8.dp,
 		) {
-			Column(
-				modifier = Modifier
-					.fillMaxWidth()
-					.padding(16.dp),
-			) {
-				MapisodeText(titleText, style = MapisodeTheme.typography.titleLarge)
-
-				MapisodeText(contentText)
+			Column(modifier = Modifier.padding(24.dp)) {
+				MapisodeText(titleText, style = MapisodeTheme.typography.headlineSmall)
 
 				Spacer(modifier = Modifier.height(16.dp))
+
+				MapisodeText(contentText, style = MapisodeTheme.typography.titleMedium)
+
+				Spacer(modifier = Modifier.height(20.dp))
 
 				Row(
 					modifier = Modifier.fillMaxWidth(),
@@ -67,8 +66,11 @@ fun MapisodeDialog(
 							},
 							indication = MapisodeRippleBIndication,
 							interactionSource = remember { MutableInteractionSource() },
-						)
+						),
+						color = MapisodeTheme.colorScheme.dialogDismiss,
+						style = MapisodeTheme.typography.labelLarge,
 					)
+					Spacer(modifier = Modifier.width(24.dp))
 					MapisodeText(
 						text = confirmText,
 						modifier = Modifier.clickable(
@@ -79,7 +81,9 @@ fun MapisodeDialog(
 							},
 							indication = MapisodeRippleBIndication,
 							interactionSource = remember { MutableInteractionSource() },
-						)
+						),
+						color = MapisodeTheme.colorScheme.dialogConfirm,
+						style = MapisodeTheme.typography.labelLarge,
 					)
 				}
 			}
