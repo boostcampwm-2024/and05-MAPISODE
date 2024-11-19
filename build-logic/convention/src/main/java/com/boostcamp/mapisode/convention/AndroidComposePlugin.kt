a566f36d7244394b334c77edcb5754c3be2eeba5
HaeTi <t9h9@naver.com>
2024-11-19 16:51:50 +0900

    add/#66 : Compose Plugin에 kotlinx-immutable 추가

--- build-logic/convention/src/main/java/com/boostcamp/mapisode/convention/AndroidComposePlugin.kt
+++ build-logic/convention/src/main/java/com/boostcamp/mapisode/convention/AndroidComposePlugin.kt
@@ -2,9 +2,13 @@ package com.boostcamp.mapisode.convention
 
 import com.android.build.gradle.LibraryExtension
 import com.boostcamp.mapisode.convention.extension.configureComposeAndroid
+import com.boostcamp.mapisode.convention.extension.getLibrary
+import com.boostcamp.mapisode.convention.extension.implementation
+import com.boostcamp.mapisode.convention.extension.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidComposePlugin : Plugin<Project> {
 	override fun apply(target: Project) {
@@ -16,6 +20,10 @@ class AndroidComposePlugin : Plugin<Project> {
 
 			extensions.configure<LibraryExtension> {
 				configureComposeAndroid(this)
+			}
+
+			dependencies {
+				implementation(libs.getLibrary("kotlinx.immutable"))
 			}
 		}
 	}
