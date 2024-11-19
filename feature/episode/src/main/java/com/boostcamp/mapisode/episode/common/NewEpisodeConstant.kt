b269b6213d083ca1507c4f14b24a258bd5b437bf
Ameri-Kano <yeokkim13@naver.com>
2024-11-19 17:31:12 +0900

    chore/#57 : ui 공통 Modifier, Arrangement 정의 및 사용

---
+++ feature/episode/src/main/java/com/boostcamp/mapisode/episode/common/NewEpisodeConstant.kt
@@ -0,0 +1,14 @@
+package com.boostcamp.mapisode.episode.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+object NewEpisodeConstant {
+	val textFieldModifier = Modifier
+		.fillMaxWidth()
+		.padding(vertical = 15.dp)
+	val textFieldVerticalArrangement = Arrangement.spacedBy(12.dp)
+}
