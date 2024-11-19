cc38802bbb90a58650068fe45b1e452d86d985fc
Ameri-Kano <yeokkim13@naver.com>
2024-11-19 18:15:59 +0900

    ui/#57 : 에피소드 내용 작성 화면 구성

--- feature/episode/src/main/java/com/boostcamp/mapisode/episode/NewEpisodeContentScreen.kt
+++ feature/episode/src/main/java/com/boostcamp/mapisode/episode/NewEpisodeContentScreen.kt
@@ -1,22 +1,28 @@
 package com.boostcamp.mapisode.episode
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
-import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
 import com.boostcamp.mapisode.designsystem.compose.MapisodeScaffold
 import com.boostcamp.mapisode.designsystem.compose.MapisodeText
 import com.boostcamp.mapisode.designsystem.compose.MapisodeTextField
+import com.boostcamp.mapisode.designsystem.compose.Surface
 import com.boostcamp.mapisode.designsystem.compose.button.MapisodeFilledButton
 import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
 import com.boostcamp.mapisode.episode.common.NewEpisodeConstant.textFieldModifier
@@ -41,87 +47,48 @@ internal fun NewEpisodeContentScreen(navController: NavController) {
 			Column {
 				Column(textFieldModifier, verticalArrangement = textFieldVerticalArrangement) {
 					MapisodeText(
-						text = stringResource(R.string.new_episode_info_location),
+						text = stringResource(R.string.new_episode_content_title),
 						style = MapisodeTheme.typography.labelLarge,
 					)
 					MapisodeTextField(
 						modifier = Modifier.fillMaxWidth(),
 						value = "",
-						placeholder = stringResource(R.string.new_episode_info_placeholder_location),
-						onValueChange = {},
-						trailingIcon = {
-							MapisodeIconButton(
-								onClick = {
-									navController.navigate("pick_location")
-								},
-							) { MapisodeIcon(com.boostcamp.mapisode.designsystem.R.drawable.ic_location) }
-						},
-					)
-				}
-				Column(textFieldModifier, verticalArrangement = textFieldVerticalArrangement) {
-					MapisodeText(
-						text = stringResource(R.string.new_episode_info_group),
-						style = MapisodeTheme.typography.labelLarge,
-					)
-					MapisodeTextField(
-						modifier = Modifier.fillMaxWidth(),
-						value = "",
-						placeholder = stringResource(R.string.new_episode_info_placeholder_group),
-						onValueChange = {},
-						trailingIcon = {
-							MapisodeIconButton(
-								onClick = {
-								},
-							) { MapisodeIcon(com.boostcamp.mapisode.designsystem.R.drawable.ic_arrow_drop_down) }
-						},
-					)
-				}
-				Column(textFieldModifier, verticalArrangement = textFieldVerticalArrangement) {
-					MapisodeText(
-						text = stringResource(R.string.new_episode_info_category),
-						style = MapisodeTheme.typography.labelLarge,
-					)
-					MapisodeTextField(
-						modifier = Modifier.fillMaxWidth(),
-						value = "",
-						placeholder = stringResource(R.string.new_episode_info_placeholder_category),
-						onValueChange = {},
-						trailingIcon = {
-							MapisodeIconButton(
-								onClick = {
-								},
-							) { MapisodeIcon(com.boostcamp.mapisode.designsystem.R.drawable.ic_arrow_drop_down) }
-						},
-					)
-				}
-				Column(textFieldModifier, verticalArrangement = textFieldVerticalArrangement) {
-					MapisodeText(
-						text = stringResource(R.string.new_episode_info_tags),
-						style = MapisodeTheme.typography.labelLarge,
-					)
-					MapisodeTextField(
-						modifier = Modifier.fillMaxWidth(),
-						value = "",
-						placeholder = stringResource(R.string.new_episode_info_placeholder_tags),
+						placeholder = stringResource(R.string.new_episode_content_placeholder_title),
 						onValueChange = {},
 					)
 				}
 				Column(textFieldModifier, verticalArrangement = textFieldVerticalArrangement) {
 					MapisodeText(
-						text = stringResource(R.string.new_episode_info_date),
+						text = stringResource(R.string.new_episode_content_description),
 						style = MapisodeTheme.typography.labelLarge,
 					)
 					MapisodeTextField(
-						modifier = Modifier.fillMaxWidth(),
+						modifier = Modifier
+							.fillMaxWidth()
+							.fillMaxHeight(0.3f),
 						value = "",
-						placeholder = stringResource(R.string.new_episode_info_placeholder_date),
+						placeholder = stringResource(R.string.new_episode_content_placeholder_description),
 						onValueChange = {},
-						trailingIcon = {
-							MapisodeIconButton(
-								onClick = {},
-							) { MapisodeIcon(com.boostcamp.mapisode.designsystem.R.drawable.ic_calendar) }
-						},
 					)
+				}
+				Column(textFieldModifier, verticalArrangement = textFieldVerticalArrangement) {
+					MapisodeText(
+						text = stringResource(R.string.new_episode_content_image),
+						style = MapisodeTheme.typography.labelLarge,
+					)
+					LazyRow(
+						contentPadding = PaddingValues(12.dp),
+						horizontalArrangement = Arrangement.spacedBy(10.dp),
+					) {
+						items(3) {
+							Surface(Modifier.size(150.dp)) {
+								Image(
+									contentDescription = null,
+									imageVector = ImageVector.vectorResource(com.boostcamp.mapisode.designsystem.R.drawable.ic_see),
+								)
+							}
+						}
+					}
 				}
 			}
 			MapisodeFilledButton(
@@ -129,7 +96,7 @@ internal fun NewEpisodeContentScreen(navController: NavController) {
 				onClick = {
 					navController.navigate("new_episode_content")
 				},
-				text = "다음",
+				text = stringResource(R.string.new_episode_create_episode),
 			)
 		}
 	}
