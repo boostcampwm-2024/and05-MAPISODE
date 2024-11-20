057fff27e484bd59186aac2124a784ad1629e998
shinythinking <shinythink@gmail.com>
2024-11-20 23:50:48 +0900

    chore/#75 : 피처 auth, 의존성 추가
    Co-authored-by: TaewoongR <iwoong98@kau.kr>

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -1,28 +1,10 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-
 plugins {
 	alias(libs.plugins.mapisode.feature)
 }
 
 android {
 	namespace = "com.boostcamp.mapisode.login"
-
-	defaultConfig {
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-		val googleWebClientId =
-			gradleLocalProperties(rootDir, providers).getProperty("GOOGLE_WEB_CLIENT_ID") ?: ""
-		if (googleWebClientId.isEmpty()) {
-			throw GradleException("GOOGLE_WEB_CLIENT_ID is not set.")
-		}
-		buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", "\"$googleWebClientId\"")
-		manifestPlaceholders["GOOGLE_WEB_CLIENT_ID"] = googleWebClientId
-	}
-
-	buildFeatures {
-		buildConfig = true
-	}
 }
-
 
 dependencies {
 	implementation(projects.domain.auth)
