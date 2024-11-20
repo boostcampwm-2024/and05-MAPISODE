568a8ae0d947810848001ea49c38cecf752dd556
shinythinking <shinythink@gmail.com>
2024-11-20 23:30:26 +0900

    feat/#75 : 도메인 auth, SignInWithGoogleUseCase 구현
    Co-authored-by: TaewoongR <iwoong98@kau.kr>

---
+++ domain/auth/src/main/java/com/boostcamp/mapisode/auth/SignInWithGoogleUseCase.kt
@@ -0,0 +1,12 @@
+package com.boostcamp.mapisode.auth
+
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SignInWithGoogleUseCase @Inject constructor(
+	private val authRepository: AuthRepository,
+) {
+	suspend operator fun invoke(): Flow<Result<User>> {
+		return authRepository.signInWithGoogle()
+	}
+}
