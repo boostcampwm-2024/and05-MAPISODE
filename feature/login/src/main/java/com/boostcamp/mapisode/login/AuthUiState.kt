1dc4bb2e42351af2717ccee411e6b00bb8e5bad6
shinythinking <shinythink@gmail.com>
2024-11-20 23:52:27 +0900

    feat/#75 : 피처 auth, AuthUiState 설정 * 성공시, User를 인수로 받도록 설정
    Co-authored-by: TaewoongR <iwoong98@kau.kr>

---
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/AuthUiState.kt
@@ -0,0 +1,10 @@
+package com.boostcamp.mapisode.login
+
+import com.boostcamp.mapisode.auth.User
+
+sealed class AuthUiState {
+	data object Initial : AuthUiState()
+	data object Loading : AuthUiState()
+	data class Success(val user: User?) : AuthUiState()
+	data class Error(val message: String) : AuthUiState()
+}
