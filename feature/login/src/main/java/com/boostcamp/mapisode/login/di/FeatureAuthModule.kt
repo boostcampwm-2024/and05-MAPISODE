53c3f4812085acf73df431403f670aac838a36cc
shinythinking <shinythink@gmail.com>
2024-11-20 23:55:34 +0900

    feat/#75 : 피처 auth, di 설정 * SignInWithGoogleUseCase 의존성 주입!
    Co-authored-by: TaewoongR <iwoong98@kau.kr>

---
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/di/FeatureAuthModule.kt
@@ -0,0 +1,21 @@
+package com.boostcamp.mapisode.login.di
+
+import com.boostcamp.mapisode.auth.AuthRepository
+import com.boostcamp.mapisode.auth.SignInWithGoogleUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FeatureAuthModule {
+	@Provides
+	@Singleton
+	fun provideSignInWithGoogleUseCase(
+		authRepository: AuthRepository,
+	): SignInWithGoogleUseCase {
+		return SignInWithGoogleUseCase(authRepository)
+	}
+}
