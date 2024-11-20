297a6dafe9a383ee09a591af438892948f939023
shinythinking <shinythink@gmail.com>
2024-11-20 23:36:59 +0900

    chore/#75 : 데이터 auth, 의존성 추가 * Local에서 Client에 대한 값을 받아오는 로직 추가
    Co-authored-by: TaewoongR <iwoong98@kau.kr>

--- data/auth/build.gradle.kts
+++ data/auth/build.gradle.kts
@@ -1,12 +1,34 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
 	alias(libs.plugins.mapisode.data)
 }
 
 android {
-	namespace = "com.boostcamp.mapisode.auth"
+	namespace = "com.boostcamp.mapisode.login"
+
+	defaultConfig {
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		val googleWebClientId =
+			gradleLocalProperties(rootDir, providers).getProperty("GOOGLE_WEB_CLIENT_ID") ?: ""
+		if (googleWebClientId.isEmpty()) {
+			throw GradleException("GOOGLE_WEB_CLIENT_ID is not set.")
+		}
+		buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", "\"$googleWebClientId\"")
+		manifestPlaceholders["GOOGLE_WEB_CLIENT_ID"] = googleWebClientId
+	}
+
+	buildFeatures {
+		buildConfig = true
+	}
 }
 
 dependencies {
 	implementation(projects.core.datastore)
 	implementation(projects.domain.auth)
+	implementation(platform(libs.firebase.bom))
+	implementation(libs.firebase.auth)
+	implementation(libs.google.play.services.auth)
+	implementation(libs.androidx.credential)
+	implementation(libs.googleid.identity)
 }
