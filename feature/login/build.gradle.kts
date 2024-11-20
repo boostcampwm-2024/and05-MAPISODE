1934d7ff5a71f0f0bee754832caa0dbc41220b4b
TaewoongR <iwoong98@kau.kr>
2024-11-20 15:48:07 +0900

    feat/#75 : 구글 oauth 의존성 추가
    - com.google.android.gms:play-services-auth
    - com.google.android.libraries.identity.googleid:googleid
    - androidx.credentials:credentials
    
    Co-authored-by: shinythinking <shinythink@gmail.com>

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -5,3 +5,12 @@ plugins {
 android {
 	namespace = "com.boostcamp.mapisode.login"
 }
+
+dependencies {
+	implementation(projects.domain.auth)
+	implementation(platform(libs.firebase.bom))
+	implementation(libs.bundles.firebase)
+	implementation(libs.google.play.services.auth)
+	implementation(libs.androidx.credential)
+	implementation(libs.googleid.identity)
+}
