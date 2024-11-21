089447775876d1bf3d79c93ca148e74e6941edc8
shinythinking <shinythink@gmail.com>
2024-11-21 16:33:45 +0900

    feature/#63 : DataStore 의존성 주입

---
+++ core/datastore/src/main/kotlin/com/boostcamp/mapisode/datastore/DataStoreModule.kt
@@ -0,0 +1,33 @@
+package com.boostcamp.mapisode.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+	@Provides
+	@Singleton
+	fun providePreferencesDataStore(
+		@ApplicationContext context: Context,
+	): DataStore<Preferences> {
+		return PreferenceDataStoreFactory.create(
+			produceFile = { context.preferencesDataStoreFile("user_preferences") },
+		)
+	}
+
+	@Provides
+	@Singleton
+	fun providePreferenceStorage(
+		dataStore: DataStore<Preferences>,
+	): UserPreferenceDataStore = UserPreferenceDataStoreImpl(dataStore)
+}
