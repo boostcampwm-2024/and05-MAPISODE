5e7e4b86ecbecc3f175b40276c049fb529c25beb
TaewoongR <iwoong98@kau.kr>
2024-11-21 15:22:32 +0900

    fix/#84 : core:auth 와 feature:login 연결
    - GoogleOauth - ViewModel- Screen 연결
    - Flow의 callbackFlow 내부에서 trySend 를 반환해야 collect 호출 시 emit

--- feature/login/src/main/java/com/boostcamp/mapisode/login/AuthUiState.kt
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/AuthUiState.kt
@@ -5,6 +5,6 @@ import com.boostcamp.mapisode.model.User
 sealed class AuthUiState {
 	data object Initial : AuthUiState()
 	data object Loading : AuthUiState()
-	data class Success(val user: User?) : AuthUiState()
+	data class Success(val user: User) : AuthUiState()
 	data class Error(val message: String) : AuthUiState()
 }
