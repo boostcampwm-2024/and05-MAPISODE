d1267f0af7a4e56bd44430e82f3f55088b7339e6
TaewoongR <iwoong98@kau.kr>
2024-11-21 14:30:14 +0900

    fix/#84 : google oauth 결과 데이터 클래스 타입 선언
    - 구글 계정에서 받은 실명과 휴대폰 번호
    - 자동 로그인을 위한 google idToken
    - firebase에서 받은 유저 구분을 위한 UID

---
+++ core/auth/src/main/java/com/boostcamp/mapisode/auth/LoginState.kt
@@ -0,0 +1,12 @@
+package com.boostcamp.mapisode.auth
+
+sealed interface LoginState {
+	data class Success(val idToken: String, val userInfo: UserInfo) : LoginState
+	data class Error(val message: String) : LoginState
+}
+
+data class UserInfo(
+	val firebaseUID: String,
+	val name: String,
+	val phoneNumber: String,
+)
