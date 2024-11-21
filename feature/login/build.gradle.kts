dfb79bfb544096baeec474e5101b703dcd90ea18
TaewoongR <iwoong98@kau.kr>
2024-11-21 15:13:57 +0900

    fix/#84 : 의존성 수정
    - feature:login 에서 auth 관련 의존성 삭제

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -9,7 +9,4 @@ android {
 dependencies {
 	implementation(projects.core.model)
 	implementation(projects.core.auth)
-	implementation(platform(libs.firebase.bom))
-	implementation(libs.bundles.firebase)
-	implementation(libs.bundles.auth)
 }
