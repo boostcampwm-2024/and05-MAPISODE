ca3f27d776860fb6bc8ea88dee9a0cb8a7b86d54
TaewoongR <iwoong98@kau.kr>
2024-11-22 21:26:21 +0900

    feat/#86 : 그룹 참여 탑앱바 백버튼 추가

--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/GroupJoinScreen.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/GroupJoinScreen.kt
@@ -1,10 +1,33 @@
 package com.boostcamp.mapisode.mygroup
 
 import androidx.compose.runtime.Composable
+import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
+import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
+import com.boostcamp.mapisode.designsystem.compose.MapisodeScaffold
+import com.boostcamp.mapisode.designsystem.compose.topbar.TopAppBar
+import com.boostcamp.mapisode.designsystem.R
 
 @Composable
 fun GroupJoinScreen(
 	onBackClick: () -> Unit,
 ) {
-
+	MapisodeScaffold(
+		isNavigationBarPaddingExist = true,
+		isStatusBarPaddingExist = true,
+		topBar = {
+			TopAppBar(
+				title = "그룹 참여",
+				navigationIcon = {
+					MapisodeIconButton(
+						onClick = onBackClick,
+					) {
+						MapisodeIcon(
+							id = R.drawable.ic_arrow_back_ios,
+							contentDescription = "Back",
+						)
+					}
+				},
+			)
+		},
+	) {}
 }
