05bf726d27067e7b7e70671f1f3a3c5f9e6f32cd
TaewoongR <iwoong98@kau.kr>
2024-11-22 21:10:30 +0900

    feat/#86 : navigate, navigator, navGraphBuilder 추가
    - NavController의 확장함수로 각 화면을 이동하는 navigate 함수들 설정
    - navGraphBuilder에 Group 화면들의 composable 추가
    - navigator 에 각 그룹 화면 이동하는 navigate 함수를 호출하는 함수 선언
    - navGraphBuilder의 확장함수로 화면 이동 콜백 인자 전달

--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/navigation/GroupNavigation.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/navigation/GroupNavigation.kt
@@ -4,17 +4,75 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.boostcamp.mapisode.mygroup.GroupRoute
+import com.boostcamp.mapisode.mygroup.GroupCreationScreen
+import com.boostcamp.mapisode.mygroup.GroupDetailScreen
+import com.boostcamp.mapisode.mygroup.GroupEditScreen
+import com.boostcamp.mapisode.mygroup.GroupJoinScreen
+import com.boostcamp.mapisode.mygroup.MainGroupRoute
+import com.boostcamp.mapisode.navigation.GroupRoute
 import com.boostcamp.mapisode.navigation.MainRoute
 
-fun NavController.navigateGroup(
+fun NavController.navigateGroupJoin(
 	navOptions: NavOptions? = null,
 ) {
-	navigate(MainRoute.Group, navOptions)
+	navigate(GroupRoute.Join, navOptions)
 }
 
-fun NavGraphBuilder.addGroupNavGraph() {
+fun NavController.navigateGroupDetail(
+	navOptions: NavOptions? = null,
+) {
+	navigate(GroupRoute.Detail, navOptions)
+}
+
+fun NavController.navigateGroupCreation(
+	navOptions: NavOptions? = null,
+) {
+	navigate(GroupRoute.Creation, navOptions)
+}
+
+fun NavController.navigateGroupEdit(
+	navOptions: NavOptions? = null,
+) {
+	navigate(GroupRoute.Edit, navOptions)
+}
+
+fun NavGraphBuilder.addGroupNavGraph(
+	onBackClick: () -> Unit,
+	onGroupJoinClick: () -> Unit,
+	onGroupDetailClick: () -> Unit,
+	onGroupCreationClick: () -> Unit,
+	onGroupEditClick: () -> Unit,
+) {
 	composable<MainRoute.Group> {
-		GroupRoute()
+		MainGroupRoute(
+			onGroupJoinClick = onGroupJoinClick,
+			onGroupDetailClick = onGroupDetailClick,
+			onGroupCreationClick = onGroupCreationClick,
+		)
+	}
+
+	composable<GroupRoute.Join> {
+		GroupJoinScreen(
+			onBackClick = onBackClick,
+		)
+	}
+
+	composable<GroupRoute.Detail> {
+		GroupDetailScreen(
+			onBackClick = onBackClick,
+			onEditClick = onGroupEditClick,
+		)
+	}
+
+	composable<GroupRoute.Creation> {
+		GroupCreationScreen(
+			onBackClick = onBackClick,
+		)
+	}
+
+	composable<GroupRoute.Edit> {
+		GroupEditScreen(
+			onBackClick = onBackClick,
+		)
 	}
 }
