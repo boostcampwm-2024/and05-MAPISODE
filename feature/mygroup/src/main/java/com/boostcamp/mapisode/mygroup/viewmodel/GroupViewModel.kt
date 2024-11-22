2f6be6b1c318ec11e6f0e1b10e777ecd758ada05
TaewoongR <iwoong98@kau.kr>
2024-11-23 03:07:45 +0900

    feat/#93 : viewmodel 선언 및 화면에 적용

---
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/viewmodel/GroupViewModel.kt
@@ -0,0 +1,49 @@
+package com.boostcamp.mapisode.mygroup.viewmodel
+
+import com.boostcamp.mapisode.model.GroupItem
+import com.boostcamp.mapisode.model.User
+import com.boostcamp.mapisode.mygroup.intent.GroupIntent
+import com.boostcamp.mapisode.mygroup.intent.GroupSideEffect
+import com.boostcamp.mapisode.mygroup.intent.GroupState
+import com.boostcamp.mapisode.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GroupViewModel @Inject constructor() :
+	BaseViewModel<GroupState, GroupSideEffect>(GroupState()) {
+
+	fun onIntent(intent: GroupIntent) {
+		when (intent) {
+			is GroupIntent.LoadGroups -> {
+				loadGroups()
+			}
+		}
+	}
+
+	private fun loadGroups() {
+		intent {
+			copy(
+				areGroupsLoading = true,
+				groups = mockItems,
+			)
+		}
+	}
+}
+
+val mockItem = GroupItem(
+	name = "Group 1",
+	imageUrl = "https://avatars.githubusercontent.com/u/127717111?v=4",
+	description = "Description for Group 1",
+	createdAt = "2021-08-01",
+	adminUser = "Admin 1",
+	users = listOf(
+		User(
+			id = "User1",
+			displayName = "DisplayName1",
+			idToken = "Token1",
+		),
+	),
+)
+
+private val mockItems: List<GroupItem> = List(20) { mockItem }
