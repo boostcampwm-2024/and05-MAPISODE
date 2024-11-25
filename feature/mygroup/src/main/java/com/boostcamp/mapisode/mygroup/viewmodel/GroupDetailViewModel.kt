3fc166a436fc6d79776ceb0f0834d0e0de20f617
TaewoongR <iwoong98@kau.kr>
2024-11-26 00:29:58 +0900

    feat/#101 : 그룹 상세 화면 뷰모델, intent, state 선언

---
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/viewmodel/GroupDetailViewModel.kt
@@ -0,0 +1,22 @@
+package com.boostcamp.mapisode.mygroup.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import com.boostcamp.mapisode.datastore.UserPreferenceDataStore
+import com.boostcamp.mapisode.mygroup.GroupRepository
+import com.boostcamp.mapisode.mygroup.intent.GroupSideEffect
+import com.boostcamp.mapisode.mygroup.state.GroupDetailState
+import com.boostcamp.mapisode.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GroupDetailViewModel @Inject constructor(
+	private val groupRepository: GroupRepository,
+	private val userPreferenceDataStore: UserPreferenceDataStore,
+	private val savedStateHandle: SavedStateHandle,
+) : BaseViewModel<GroupDetailState, GroupSideEffect>(GroupDetailState()) {
+
+	fun getGroupDetail(groupId: String) {
+
+	}
+}
