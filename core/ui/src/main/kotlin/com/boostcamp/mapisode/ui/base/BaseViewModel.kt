a824a85020ffd3a5899ec9d30eedcc98d749fddc
HaeTi <t9h9@naver.com>
2024-11-26 00:20:49 +0900

    refactor/#103 : BaseViewModel 리펙토링 - onIntent를 강제 - Channel 버퍼 크기 64로 감소 - trySend -> send

--- core/ui/src/main/kotlin/com/boostcamp/mapisode/ui/base/BaseViewModel.kt
+++ core/ui/src/main/kotlin/com/boostcamp/mapisode/ui/base/BaseViewModel.kt
@@ -1,36 +1,37 @@
 package com.boostcamp.mapisode.ui.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
-import timber.log.Timber
+import kotlinx.coroutines.launch
 
-abstract class BaseViewModel<UI_STATE : UiState, SIDE_EFFECT : SideEffect>(
+abstract class BaseViewModel<UI_INTENT : UiIntent, UI_STATE : UiState, SIDE_EFFECT : SideEffect>(
 	initialState: UI_STATE,
 ) : ViewModel() {
 
 	private val _uiState = MutableStateFlow(initialState)
 	val uiState = _uiState.asStateFlow()
 
-	private val _sideEffect: Channel<SIDE_EFFECT> = Channel(Channel.UNLIMITED)
+	private val _sideEffect: Channel<SIDE_EFFECT> = Channel(Channel.BUFFERED)
 	val sideEffect = _sideEffect.receiveAsFlow()
 
 	protected val currentState: UI_STATE
 		get() = _uiState.value
+
+	abstract fun onIntent(intent: UI_INTENT)
 
 	protected fun intent(reduce: UI_STATE.() -> UI_STATE) {
 		_uiState.update { currentState.reduce() }
 	}
 
 	protected fun postSideEffect(vararg effects: SIDE_EFFECT) {
-		effects.forEach { effect ->
-			val result = _sideEffect.trySend(effect)
-			if (result.isFailure) {
-				// TODO : 에러 로직처리 방법 결정 후 수정
-				Timber.e(result.exceptionOrNull())
+		viewModelScope.launch {
+			effects.forEach { effect ->
+				_sideEffect.send(effect)
 			}
 		}
 	}
