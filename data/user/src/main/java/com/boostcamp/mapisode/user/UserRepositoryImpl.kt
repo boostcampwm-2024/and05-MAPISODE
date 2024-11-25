6a1024ca9a4861ea553952721709e4fa006a36c0
shinythinking <shinythink@gmail.com>
2024-11-26 00:24:37 +0900

    refactor/#105 : 커스텀 데이터 클래스를 사용하여 사용자 추가

--- data/user/src/main/java/com/boostcamp/mapisode/user/UserRepositoryImpl.kt
+++ data/user/src/main/java/com/boostcamp/mapisode/user/UserRepositoryImpl.kt
@@ -3,34 +3,25 @@ package com.boostcamp.mapisode.user
 import com.boostcamp.mapisode.firebase.firestore.FirestoreConstants
 import com.boostcamp.mapisode.model.UserModel
 import com.boostcamp.mapisode.user.model.toUserFirestoreModel
-import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
-import timber.log.Timber
 import javax.inject.Inject
 
-class UserRepositoryImpl @Inject constructor(private val database: FirebaseFirestore) :
+class UserRepositoryImpl @Inject constructor(database: FirebaseFirestore) :
 	UserRepository {
 	private val userCollection = database.collection(FirestoreConstants.COLLECTION_USER)
 
 	override suspend fun createUser(userModel: UserModel) {
-		val userFirestoreModel = userModel.toUserFirestoreModel(database)
-		Timber.d("userFirestoreModel: $userFirestoreModel")
-		val userData = mapOf(
-			"nickname" to userFirestoreModel.nickname,
-			"email" to userFirestoreModel.email,
-			"profileUri" to userFirestoreModel.profileUri,
-			"joinedAt" to FieldValue.serverTimestamp(),
-		)
+		val userFirestoreModel = userModel.toUserFirestoreModel()
+
 		val userDocument = userCollection.document(userFirestoreModel.uid)
-		Timber.d("userDocument: $userDocument")
 
 		try {
 			val document = userDocument.get().await()
 			if (document.exists()) {
 				throw Exception("User already exists")
 			}
-			userDocument.set(userData).await()
+			userDocument.set(userFirestoreModel).await()
 		} catch (e: Exception) {
 			throw Exception("Failed to create user", e)
 		}
