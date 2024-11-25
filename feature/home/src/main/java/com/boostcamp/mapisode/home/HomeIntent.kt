c430b5cace25547099aa06dd1ed095eb557666b0
HaeTi <t9h9@naver.com>
2024-11-26 00:22:48 +0900

    refactor/#103 : BaseViewModel 변경 사항 대응

--- feature/home/src/main/java/com/boostcamp/mapisode/home/HomeIntent.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/HomeIntent.kt
@@ -2,9 +2,10 @@ package com.boostcamp.mapisode.home
 
 import com.boostcamp.mapisode.home.common.ChipType
 import com.boostcamp.mapisode.model.EpisodeLatLng
+import com.boostcamp.mapisode.ui.base.UiIntent
 import com.naver.maps.geometry.LatLng
 
-sealed class HomeIntent {
+sealed class HomeIntent : UiIntent {
 	data object RequestLocationPermission : HomeIntent()
 	data class SetInitialLocation(val latLng: LatLng) : HomeIntent()
 	data class UpdateLocationPermission(val isGranted: Boolean) : HomeIntent() // 위치 권한 설정 여부 업데이트
--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/intent/GroupIntent.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/intent/GroupIntent.kt
@@ -1,9 +1,10 @@
 package com.boostcamp.mapisode.mygroup.intent
 
 import androidx.compose.runtime.Immutable
+import com.boostcamp.mapisode.ui.base.UiIntent
 
 @Immutable
-sealed class GroupIntent {
+sealed class GroupIntent : UiIntent {
 	data object LoadGroups : GroupIntent()
 	data object EndLoadingGroups : GroupIntent()
 }
