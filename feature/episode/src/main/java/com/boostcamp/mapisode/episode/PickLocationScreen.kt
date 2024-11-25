1e88d4c680ec37bdf9e7abae6998f0069f6959d7
Ameri-Kano <yeokkim13@naver.com>
2024-11-25 22:25:04 +0900

    feat/#100 : 장소 선택 지도에서 위치 이동 시 디바운스 처리 추가

--- feature/episode/src/main/java/com/boostcamp/mapisode/episode/PickLocationScreen.kt
+++ feature/episode/src/main/java/com/boostcamp/mapisode/episode/PickLocationScreen.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
@@ -29,8 +33,12 @@ import com.naver.maps.map.compose.Marker
 import com.naver.maps.map.compose.NaverMap
 import com.naver.maps.map.compose.rememberFusedLocationSource
 import com.naver.maps.map.compose.rememberMarkerState
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
 
-@OptIn(ExperimentalNaverMapApi::class)
+@OptIn(ExperimentalNaverMapApi::class, FlowPreview::class)
 @Composable
 internal fun PickLocationScreen(
 	cameraPositionState: CameraPositionState,
@@ -39,6 +47,18 @@ internal fun PickLocationScreen(
 ) {
 	val episodeMarkerState = rememberMarkerState()
 	episodeMarkerState.position = cameraPositionState.position.target
+	val locationStateFlow = remember {
+		MutableStateFlow(cameraPositionState.position.target)
+	}
+	val location by locationStateFlow.collectAsStateWithLifecycle()
+
+	LaunchedEffect(cameraPositionState.position.target) {
+		locationStateFlow
+			.debounce(500L)
+			.collectLatest {
+				locationStateFlow.value = cameraPositionState.position.target
+			}
+	}
 
 	MapisodeScaffold(
 		isStatusBarPaddingExist = true,
@@ -82,7 +102,7 @@ internal fun PickLocationScreen(
 						style = MapisodeTheme.typography.headlineSmall,
 					)
 					MapisodeText(
-						text = stringResource(R.string.new_episode_info_placeholder_location),
+						text = latLngString(location),
 						style = MapisodeTheme.typography.bodyLarge,
 					)
 					MapisodeFilledButton(
