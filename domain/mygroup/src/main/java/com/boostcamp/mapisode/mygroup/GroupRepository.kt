8dd0acf472acd1517b275072731b4c01b2226289
TaewoongR <iwoong98@kau.kr>
2024-11-25 12:11:55 +0900

    fix/#95 : repository 그룹 get 함수 수정
    - 모든 그룹 -> 유저 id가 속한 그룹

--- domain/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/GroupRepository.kt
+++ domain/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/GroupRepository.kt
@@ -3,8 +3,7 @@ package com.boostcamp.mapisode.mygroup
 import com.boostcamp.mapisode.model.GroupModel
 
 interface GroupRepository {
-	suspend fun getGroupById(groupId: String): GroupModel?
-	suspend fun getAllGroups(): List<GroupModel>
+	suspend fun getGroupsByUserId(userId: String): List<GroupModel>
 	suspend fun createGroup(groupModel: GroupModel): String
 	suspend fun updateGroup(groupId: String, groupModel: GroupModel)
 	suspend fun deleteGroup(groupId: String)
