cbf3ba625d63249696b42763fda4c2481bbbfd53
shinythinking <shinythink@gmail.com>
2024-11-25 22:24:39 +0900

    refactor/#105 : UserRepository와 EpisodeRepository 모듈 분리를 위한 삭제

--- data/episode/src/main/java/com/boostcamp/mapisode/episode/model/UserFirestoreModel.kt
+++
@@ -1,8 +0,0 @@
-package com.boostcamp.mapisode.episode.model
-
-data class UserFirestoreModel(
-	val uid: String,
-	val nickname: String,
-	val email: String,
-	val profileUri: String,
-)
--- domain/episode/src/main/java/com/boostcamp/mapisode/user/UserRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.boostcamp.mapisode.user
-
-import com.boostcamp.mapisode.model.UserModel
-
-interface UserRepository {
-	suspend fun createUser(userModel: UserModel)
-}
