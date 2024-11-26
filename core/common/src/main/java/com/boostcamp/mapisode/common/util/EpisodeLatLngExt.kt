8e85736ca38004caa13a57653f6535db81888cff
HaeTi <t9h9@naver.com>
2024-11-27 05:51:42 +0900

    fix/#103 : ktlint - single expression

--- core/common/src/main/java/com/boostcamp/mapisode/common/util/EpisodeLatLngExt.kt
+++ core/common/src/main/java/com/boostcamp/mapisode/common/util/EpisodeLatLngExt.kt
@@ -3,8 +3,7 @@ package com.boostcamp.mapisode.common.util
 import com.boostcamp.mapisode.model.EpisodeLatLng
 import com.naver.maps.geometry.LatLng
 
-fun EpisodeLatLng.distanceTo(other: EpisodeLatLng): Double {
-	return calculateDistance(this.latitude, this.longitude, other.latitude, other.longitude)
-}
+fun EpisodeLatLng.distanceTo(other: EpisodeLatLng) =
+	calculateDistance(this.latitude, this.longitude, other.latitude, other.longitude)
 
 fun EpisodeLatLng.toLatLng() = LatLng(this.latitude, this.longitude)
