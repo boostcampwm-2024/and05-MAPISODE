0ce8f96542af2341214159fb86929c67182fa82c
shinythinking <shinythink@gmail.com>
2024-11-26 13:32:01 +0900

    feat/#91 : AuthViewModel 생성
    * BaseViewModel을 상속
    * onIntent 구현

--- feature/login/src/main/java/com/boostcamp/mapisode/login/AuthViewModel.kt
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/AuthViewModel.kt
@@ -1,44 +1,27 @@
 package com.boostcamp.mapisode.login
 
-import android.content.Context
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.boostcamp.mapisode.auth.GoogleOauth
-import com.boostcamp.mapisode.auth.LoginState
-import com.boostcamp.mapisode.model.AuthData
+import com.boostcamp.mapisode.datastore.UserPreferenceDataStore
+import com.boostcamp.mapisode.ui.base.BaseViewModel
+import com.boostcamp.mapisode.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AuthViewModel @Inject constructor() : ViewModel() {
+class AuthViewModel @Inject constructor(
+	private val userRepository: UserRepository,
+	private val userDataStore: UserPreferenceDataStore,
+) : BaseViewModel<AuthIntent, AuthState, AuthSideEffect>(AuthState()) {
 
-	private val _uiState = MutableStateFlow<AuthUiState>(AuthUiState.Initial)
-	val uiState = _uiState.asStateFlow()
+	override fun onIntent(intent: AuthIntent) {
+		when (intent) {
+			is AuthIntent.OnGoogleSignInClick -> handleGoogleSignIn(intent.googleOauth)
 
-	fun handleGoogleSignIn(context: Context) {
-		viewModelScope.launch {
-			_uiState.value = AuthUiState.Loading
-			GoogleOauth(context).googleSignIn().collect { result ->
-				_uiState.value = when (result) {
-					is LoginState.Success -> {
-						AuthUiState.Success(
-							AuthData(
-								uid = result.authDataInfo.uid,
-								displayName = result.authDataInfo.displayName,
-								idToken = result.idToken,
-								email = result.authDataInfo.email,
-							),
-						)
-					}
-
-					is LoginState.Error -> {
-						AuthUiState.Error("Sign-in failed")
-					}
-				}
+			is AuthIntent.OnNicknameChange -> {
+				onNicknameChange(intent.nickname)
 			}
+
+			is AuthIntent.OnSignUpClick -> handleSignUp()
 		}
 	}
+
 }
