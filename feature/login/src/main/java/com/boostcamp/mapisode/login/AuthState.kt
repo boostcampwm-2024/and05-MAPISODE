a2416f3079f58f1f7260cf5d0afd78113136d8e2
shinythinking <shinythink@gmail.com>
2024-11-26 13:15:05 +0900

    feat/#91 : AuthState 구성

---
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/AuthState.kt
@@ -0,0 +1,12 @@
+package com.boostcamp.mapisode.login
+
+import com.boostcamp.mapisode.model.AuthData
+import com.boostcamp.mapisode.ui.base.UiState
+
+data class AuthState(
+	val isLoginSuccess: Boolean = false,
+	val authData: AuthData? = null,
+	val nickname: String = "",
+	val profileUri: String = "",
+	val isNicknameValid: Boolean = false,
+) : UiState
