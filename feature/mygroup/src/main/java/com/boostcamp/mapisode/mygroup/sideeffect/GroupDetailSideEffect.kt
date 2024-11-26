8ba7356cb1d9426fc8671301e6edeb8828bdc2bf
TaewoongR <iwoong98@kau.kr>
2024-11-26 16:18:58 +0900

    feat/#101 : GroupDetailSideEffect 확장

--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/sideeffect/GroupDetailSideEffect.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/sideeffect/GroupDetailSideEffect.kt
@@ -5,5 +5,9 @@ import com.boostcamp.mapisode.ui.base.SideEffect
 
 @Immutable
 sealed class GroupDetailSideEffect : SideEffect {
+	data object Idle : GroupJoinSideEffect()
 	data class ShowToast(val message: String) : GroupDetailSideEffect()
+	data object NavigateToGroupEditScreen : GroupDetailSideEffect()
+	data object NavigateToGroupScreen : GroupDetailSideEffect()
+	data class NavigateToEpisode(val episodeId: String) : GroupDetailSideEffect()
 }
