a61cb741f5ada6a1ec291a0325b8842d74efc640
shinythinking <shinythink@gmail.com>
2024-11-26 13:35:47 +0900

    feat/#91 : ViewModel과 AuthRoute 연동
    * 하나의 뷰모델을 두 화면이 공유합니다.

--- feature/login/src/main/java/com/boostcamp/mapisode/login/AuthRoute.kt
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/AuthRoute.kt
@@ -1,6 +1,8 @@
 package com.boostcamp.mapisode.login
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -16,19 +18,31 @@ fun AuthRoute(
 	val context = LocalContext.current
 	val googleOauth = GoogleOauth(context)
 
-	// 빌드 통과하게 만들기 위해 추가한 더미 코드
-	val isLoginSuccess = true
-	val nickname = "nickname"
+	LaunchedEffect(Unit) {
+		viewModel.sideEffect.collect { effect ->
+			when (effect) {
+				is AuthSideEffect.NavigateToMain -> navigateToMain()
 
-	if (isLoginSuccess) {
+				is AuthSideEffect.ShowError -> {
+					Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show()
+				}
+			}
+		}
+	}
+
+	if (uiState.isLoginSuccess) {
 		SignUpScreen(
-			nickname = nickname,
-			onNicknameChanged = { },
-			onSignUpClick = { },
+			nickname = uiState.nickname,
+			onNicknameChanged = { newNickname ->
+				viewModel.onIntent(AuthIntent.OnNicknameChange(newNickname))
+			},
+			onSignUpClick = { viewModel.onIntent(AuthIntent.OnSignUpClick) },
 		)
 	} else {
 		LoginScreen(
-			googleSignInClicked = { },
+			googleSignInClicked = {
+				viewModel.onIntent(AuthIntent.OnGoogleSignInClick(googleOauth))
+			},
 		)
 	}
 }
