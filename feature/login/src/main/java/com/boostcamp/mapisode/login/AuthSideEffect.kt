5e5e4f01c4bb7d9af4cb28ec5396ad963505293b
shinythinking <shinythink@gmail.com>
2024-11-26 13:29:41 +0900

    feat/#91 : AuthSideEffect 구성

---
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/AuthSideEffect.kt
@@ -0,0 +1,8 @@
+package com.boostcamp.mapisode.login
+
+import com.boostcamp.mapisode.ui.base.SideEffect
+
+sealed interface AuthSideEffect : SideEffect {
+	data object NavigateToMain : AuthSideEffect
+	data class ShowError(val message: String) : AuthSideEffect
+}
