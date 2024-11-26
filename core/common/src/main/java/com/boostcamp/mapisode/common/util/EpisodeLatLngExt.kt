80a20c5d3a842133dad14e4578b02b6de9ef4dc4
HaeTi <t9h9@naver.com>
2024-11-27 03:24:28 +0900

    feat/#103 : haversine으로 두 위치 사이 거리 구하기 구현

---
+++ core/common/src/main/java/com/boostcamp/mapisode/common/util/EpisodeLatLngExt.kt
@@ -0,0 +1,10 @@
+package com.boostcamp.mapisode.common.util
+
+import com.boostcamp.mapisode.model.EpisodeLatLng
+import com.naver.maps.geometry.LatLng
+
+fun EpisodeLatLng.distanceTo(other: EpisodeLatLng): Double {
+	return calculateDistance(this.latitude, this.longitude, other.latitude, other.longitude)
+}
+
+fun EpisodeLatLng.toLatLng() = LatLng(this.latitude, this.longitude)
