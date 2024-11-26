cdb1b197372edd0f029f06ca81adca848aae6340
TaewoongR <iwoong98@kau.kr>
2024-11-27 04:06:22 +0900

    feat/#101 : 그룹 편집 화면 기능 적용
    - 1. 편집화면으로 이동하면 그룹 이미지, 제목, 내용이 뜹니다.
    - 2. 제목과 내용을 수정하고 버튼을 누르면 수정된 내용을 바탕으로 firestore에 업데이트(덮어씌우기)를 요청합니다.
    - 3. 성공하면 이전 화면으로, 실패시 토스트 메세지를 띄웁니다.

--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/screen/GroupEditScreen.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/screen/GroupEditScreen.kt
@@ -1,12 +1,19 @@
 package com.boostcamp.mapisode.mygroup.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -17,18 +24,29 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.boostcamp.mapisode.designsystem.R
+import com.boostcamp.mapisode.designsystem.compose.Direction
+import com.boostcamp.mapisode.designsystem.compose.MapisodeDivider
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
 import com.boostcamp.mapisode.designsystem.compose.MapisodeScaffold
+import com.boostcamp.mapisode.designsystem.compose.MapisodeText
+import com.boostcamp.mapisode.designsystem.compose.MapisodeTextField
+import com.boostcamp.mapisode.designsystem.compose.Thickness
+import com.boostcamp.mapisode.designsystem.compose.button.MapisodeFilledButton
 import com.boostcamp.mapisode.designsystem.compose.button.MapisodeImageButton
 import com.boostcamp.mapisode.designsystem.compose.topbar.TopAppBar
+import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
+import com.boostcamp.mapisode.model.GroupModel
 import com.boostcamp.mapisode.mygroup.intent.GroupEditIntent
 import com.boostcamp.mapisode.mygroup.sideeffect.GroupEditSideEffect
 import com.boostcamp.mapisode.mygroup.sideeffect.rememberFlowWithLifecycle
+import com.boostcamp.mapisode.mygroup.state.GroupEditState
 import com.boostcamp.mapisode.mygroup.viewmodel.GroupEditViewModel
 import com.boostcamp.mapisode.navigation.GroupRoute
 
@@ -45,7 +63,7 @@ fun GroupEditScreen(
 	).value
 
 	LaunchedEffect(uiState.value) {
-		if(uiState.value.isInitializing) {
+		if (uiState.value.isInitializing) {
 			viewModel.onIntent(GroupEditIntent.LoadGroups(edit.groupId))
 		}
 	}
@@ -59,13 +77,25 @@ fun GroupEditScreen(
 	}
 
 	GroupEditContent(
+		uiState = uiState.value,
 		onBackClick = onBackClick,
+		onGroupEditClick = { title, content, imageUrl ->
+			viewModel.onIntent(
+				GroupEditIntent.OnGroupEditClick(
+					title = title,
+					content = content,
+					imageUrl = imageUrl,
+				),
+			)
+		},
 	)
 }
 
 @Composable
 fun GroupEditContent(
+	uiState: GroupEditState,
 	onBackClick: () -> Unit,
+	onGroupEditClick: (title: String, content: String, imageUrl: String) -> Unit,
 ) {
 	val focusManager = LocalFocusManager.current
 
@@ -80,6 +110,7 @@ fun GroupEditContent(
 				)
 			},
 		isStatusBarPaddingExist = true,
+		isNavigationBarPaddingExist = true,
 		topBar = {
 			TopAppBar(
 				title = "그룹 편집",
@@ -96,29 +127,139 @@ fun GroupEditContent(
 				},
 			)
 		},
+	) { paddingValues ->
+		if (uiState.group != null) {
+			GroupEditField(
+				paddingValues = paddingValues,
+				group = uiState.group,
+				onGroupEditClick = onGroupEditClick,
+			)
+		}
+	}
+}
+
+@Composable
+fun GroupEditField(
+	paddingValues: PaddingValues,
+	group: GroupModel,
+	onGroupEditClick: (title: String, content: String, imageUrl: String) -> Unit,
+) {
+	var name by remember { mutableStateOf(group.name) }
+	var description by remember { mutableStateOf(group.description) }
+	var profileUrl by remember { mutableStateOf(group.imageUrl) }
+
+	Box(
+		modifier = Modifier
+			.fillMaxSize()
+			.padding(paddingValues)
+			.padding(horizontal = 20.dp),
 	) {
-		var isClicked by remember { mutableStateOf(true) }
+		LazyColumn(
+			modifier = Modifier
+				.fillMaxSize(),
+			horizontalAlignment = Alignment.CenterHorizontally,
+			verticalArrangement = Arrangement.spacedBy(20.dp),
+			contentPadding = PaddingValues(vertical = 10.dp),
+		) {
+			item {
+				Column {
+					MapisodeText(
+						text = "그룹 대표 이미지",
+						style = MapisodeTheme.typography.titleMedium
+							.copy(fontWeight = FontWeight.SemiBold),
+					)
+
+					Spacer(modifier = Modifier.padding(4.dp))
+
+					MapisodeImageButton(
+						modifier = Modifier
+							.sizeIn(maxWidth = 380.dp, maxHeight = 380.dp)
+							.fillMaxWidth()
+							.aspectRatio(1f),
+						onClick = { },
+						showImage = false,
+						text = "이미지를 선택하세요",
+					) {
+						AsyncImage(
+							model = profileUrl,
+							contentDescription = "그룹 이미지",
+							modifier = Modifier.fillMaxSize(),
+						)
+					}
+				}
+			}
+			item {
+				Column(
+					modifier = Modifier
+						.sizeIn(maxWidth = 380.dp, maxHeight = 380.dp)
+						.fillMaxWidth(),
+				) {
+					MapisodeText(
+						text = "이름",
+						style = MapisodeTheme.typography.titleMedium
+							.copy(fontWeight = FontWeight.SemiBold),
+					)
+
+					Spacer(modifier = Modifier.padding(4.dp))
+
+					MapisodeTextField(
+						value = name,
+						onValueChange = {
+							name = it
+						},
+						modifier = Modifier.fillMaxWidth(),
+					)
+				}
+			}
+			item {
+				Column(
+					modifier = Modifier
+						.sizeIn(maxWidth = 380.dp, maxHeight = 380.dp)
+						.fillMaxWidth(),
+				) {
+					MapisodeText(
+						text = "설명",
+						style = MapisodeTheme.typography.titleMedium
+							.copy(fontWeight = FontWeight.SemiBold),
+					)
+
+					Spacer(modifier = Modifier.padding(4.dp))
+
+					MapisodeTextField(
+						value = description,
+						onValueChange = {
+							description = it
+						},
+						modifier = Modifier
+							.heightIn(min = 100.dp, max = 300.dp)
+							.fillMaxWidth(),
+					)
+
+					Spacer(modifier = Modifier.padding(40.dp))
+				}
+			}
+		}
 		Column(
 			modifier = Modifier
-				.fillMaxSize()
-				.padding(it),
-			verticalArrangement = Arrangement.Center,
+				.fillMaxWidth()
+				.align(Alignment.BottomCenter)
+				.background(MapisodeTheme.colorScheme.surfaceBackground),
 			horizontalAlignment = Alignment.CenterHorizontally,
 		) {
-			MapisodeImageButton(
+			Spacer(modifier = Modifier.padding(top = 4.dp))
+			MapisodeDivider(direction = Direction.Horizontal, thickness = Thickness.Thin)
+			Spacer(modifier = Modifier.padding(5.dp))
+			MapisodeFilledButton(
 				modifier = Modifier
-					.fillMaxWidth(0.8f)
-					.aspectRatio(1f),
-				onClick = { isClicked = !isClicked },
-				showImage = isClicked,
-				text = "이미지를 선택하세요",
-			) {
-				AsyncImage(
-					model = "https://m.media-amazon.com/images/I/61jyqnlyIaS.jpg",
-					contentDescription = "그룹 이미지",
-					modifier = Modifier.fillMaxSize(),
-				)
-			}
+					.sizeIn(maxWidth = 380.dp, maxHeight = 380.dp)
+					.fillMaxWidth()
+					.heightIn(52.dp),
+				onClick = {
+					onGroupEditClick(name, description, profileUrl)
+				},
+				text = "편집하기",
+				showRipple = true,
+			)
 		}
 	}
 }
