2844f9ffc60e6a91f3683e3e4b7992b8110a9df8
shinythinking <shinythink@gmail.com>
2024-11-26 13:14:34 +0900

    feat/#91 : AuthIntent 구성

---
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/AuthIntent.kt
@@ -0,0 +1,10 @@
+package com.boostcamp.mapisode.login
+
+import com.boostcamp.mapisode.auth.GoogleOauth
+import com.boostcamp.mapisode.ui.base.UiIntent
+
+sealed interface AuthIntent : UiIntent {
+	data class OnGoogleSignInClick(val googleOauth: GoogleOauth) : AuthIntent
+	data class OnNicknameChange(val nickname: String) : AuthIntent
+	data object OnSignUpClick : AuthIntent
+}
