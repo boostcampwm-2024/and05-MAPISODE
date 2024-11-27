952704c0337863a51706b8142fd07f8d458cd066
shinythinking <shinythink@gmail.com>
2024-11-27 17:17:30 +0900

    refactor/#122 : Firestore 유저 모델 변경

--- data/user/src/main/java/com/boostcamp/mapisode/user/model/UserFirestoreModel.kt
+++ data/user/src/main/java/com/boostcamp/mapisode/user/model/UserFirestoreModel.kt
@@ -1,8 +1,12 @@
 package com.boostcamp.mapisode.user.model
 
+import com.boostcamp.mapisode.model.GroupModel
+import java.util.Date
+
 data class UserFirestoreModel(
-	val uid: String,
-	val nickname: String,
+	val name: String,
 	val email: String,
-	val profileUri: String,
+	val profileUrl: String,
+	val joinedAt: Date = Date.from(java.time.Instant.now()),
+	val groups: List<GroupModel> = emptyList(),
 )
