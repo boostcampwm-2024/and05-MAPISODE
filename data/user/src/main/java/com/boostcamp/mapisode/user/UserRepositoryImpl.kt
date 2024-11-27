9717c87d082625d705f7a7493f0a93e79cf1dd79
shinythinking <shinythink@gmail.com>
2024-11-27 17:03:16 +0900

    feat/#122 : firestore에서 유저 정보 가져오는 함수 구현

--- data/user/src/main/java/com/boostcamp/mapisode/user/UserRepositoryImpl.kt
+++ data/user/src/main/java/com/boostcamp/mapisode/user/UserRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.boostcamp.mapisode.model.UserModel
 import com.boostcamp.mapisode.user.model.toUserFirestoreModel
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
+import java.util.Date
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(database: FirebaseFirestore) : UserRepository {
@@ -13,16 +14,34 @@ class UserRepositoryImpl @Inject constructor(database: FirebaseFirestore) : User
 	override suspend fun createUser(userModel: UserModel) {
 		val userFirestoreModel = userModel.toUserFirestoreModel()
 
-		val userDocument = userCollection.document(userFirestoreModel.uid)
+		val userDocument = userCollection.document(userModel.uid)
 
 		try {
 			val document = userDocument.get().await()
-			if (document.exists()) {
-				throw Exception("User already exists")
-			}
+
+			if (document.exists()) throw Exception("User already exists")
+
 			userDocument.set(userFirestoreModel).await()
 		} catch (e: Exception) {
 			throw Exception("Failed to create user", e)
+		}
+	}
+
+	override suspend fun getUserInfo(uid: String): UserModel {
+		val userDocument = userCollection.document(uid)
+
+		try {
+			val user = userDocument.get().await()
+			return UserModel(
+				uid = user.id,
+				name = user.getString("name") ?: "",
+				email = user.getString("email") ?: "",
+				profileUrl = user.getString("profileUrl") ?: "",
+				joinedAt = user.getDate("joinedAt") ?: Date.from(java.time.Instant.now()),
+				groups = emptyList(), // 우선 빈 리스트를 넣도록 했습니다. 혹시나 사용하실 분은 수정해주세요.
+			)
+		} catch (e: Exception) {
+			throw Exception("Failed to get user", e)
 		}
 	}
 
