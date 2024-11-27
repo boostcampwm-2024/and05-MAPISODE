9d8aa2ee4b2cfe0b8c4790e5b77ad9e794bd000d
HaeTi <t9h9@naver.com>
2024-11-28 04:28:09 +0900

    feat/#125 : 에피소드 상세 - 콘텐츠 텍스트 추가

--- feature/home/src/main/java/com/boostcamp/mapisode/home/common/EpisodeDetailPreviewProvider.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/common/EpisodeDetailPreviewProvider.kt
@@ -49,6 +49,7 @@ class EpisodeDetailPreviewProvider : PreviewParameterProvider<EpisodeDetailState
 					"태그10",
 				),
 				category = "EAT",
+				content = "파이브가이즈 다녀왔는데 정말 만족스러웠어! \uD83C\uDF54 매장에 들어서자마자 미국 느낌이 확 나서 신기했고, 오픈 주방에서 직접 버거 굽고 준비하는 모습 보니까 신선하고 믿음이 가더라구. 주문할 때 토핑을 내 맘대로 추가할 수 있어서 완전 내 스타일로 커스텀할 수 있었어. 버거는 손에 딱 잡히는 묵직한 느낌이 좋았고, 한 입 베어 물자마자 육즙이 팡 터지는데 진짜 맛있었어! 감자튀김도 양이 푸짐하고 바삭해서 버거랑 환상 조합이더라. \uD83C\uDF5F",
 			),
 			author = UserModel(
 				name = "Author",
