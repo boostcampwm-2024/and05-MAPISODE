42b98505df4b1244ba994545e7289288901512fd
HaeTi <t9h9@naver.com>
2024-11-28 03:08:14 +0900

    feat/#125 : 에피소드 상세 - 사진 row 구현

--- feature/home/src/main/java/com/boostcamp/mapisode/home/detail/EpisodeDetailScreen.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/detail/EpisodeDetailScreen.kt
@@ -1,23 +1,34 @@
 package com.boostcamp.mapisode.home.detail
 
 import android.widget.Toast
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
 import com.boostcamp.mapisode.designsystem.R
 import com.boostcamp.mapisode.designsystem.compose.IconSize
 import com.boostcamp.mapisode.designsystem.compose.MapisodeCircularLoadingIndicator
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
 import com.boostcamp.mapisode.designsystem.compose.MapisodeScaffold
-import com.boostcamp.mapisode.designsystem.compose.MapisodeText
 import com.boostcamp.mapisode.designsystem.compose.topbar.TopAppBar
 
 @Composable
@@ -89,6 +100,31 @@ internal fun EpisodeDetailScreen(
 			)
 		},
 	) {
-		MapisodeText(text = "EpisodeDetailScreen")
+		Column(modifier = modifier.fillMaxSize()) {
+			val imageUrls = state.episode.imageUrls
+
+			Box(
+				modifier = Modifier.fillMaxWidth(),
+				contentAlignment = Alignment.Center,
+			) {
+				Row(
+					modifier = Modifier
+						.wrapContentWidth()
+						.horizontalScroll(rememberScrollState()),
+					horizontalArrangement = Arrangement.spacedBy(4.dp),
+					verticalAlignment = Alignment.CenterVertically,
+				) {
+					imageUrls.forEach { imageUrl ->
+						AsyncImage(
+							model = imageUrl,
+							contentDescription = "애피소드 이미지",
+							modifier = Modifier
+								.size(110.dp)
+								.clip(RoundedCornerShape(16.dp)),
+						)
+					}
+				}
+			}
+		}
 	}
 }
