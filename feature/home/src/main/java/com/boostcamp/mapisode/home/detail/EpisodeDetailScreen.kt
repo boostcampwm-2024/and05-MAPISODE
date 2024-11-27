80ebfcde2afa3c359ea0751862ad350f2bde4965
HaeTi <t9h9@naver.com>
2024-11-28 03:22:06 +0900

    feat/#125 : 에피소드 상세 - PreviewParameterProvider 활용

--- feature/home/src/main/java/com/boostcamp/mapisode/home/detail/EpisodeDetailScreen.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/detail/EpisodeDetailScreen.kt
@@ -8,21 +8,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePreviewHandler
+import coil3.compose.LocalAsyncImagePreviewHandler
+import coil3.test.FakeImage
 import com.boostcamp.mapisode.designsystem.R
 import com.boostcamp.mapisode.designsystem.compose.IconSize
 import com.boostcamp.mapisode.designsystem.compose.MapisodeCircularLoadingIndicator
@@ -30,6 +38,7 @@ import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
 import com.boostcamp.mapisode.designsystem.compose.MapisodeScaffold
 import com.boostcamp.mapisode.designsystem.compose.topbar.TopAppBar
+import com.boostcamp.mapisode.home.common.EpisodeDetailPreviewProvider
 
 @Composable
 internal fun EpisodeDetailRoute(
@@ -100,7 +109,11 @@ internal fun EpisodeDetailScreen(
 			)
 		},
 	) {
-		Column(modifier = modifier.fillMaxSize()) {
+		Column(
+			modifier = modifier
+				.fillMaxSize()
+				.padding(it),
+		) {
 			val imageUrls = state.episode.imageUrls
 
 			Box(
@@ -126,5 +139,20 @@ internal fun EpisodeDetailScreen(
 				}
 			}
 		}
+	}
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun EpisodeDetailScreenPreview(
+	@PreviewParameter(EpisodeDetailPreviewProvider::class) state: EpisodeDetailState,
+) {
+	val previewHandler = AsyncImagePreviewHandler {
+		FakeImage(color = 0xFFE0E0E0.toInt())
+	}
+
+	CompositionLocalProvider(LocalAsyncImagePreviewHandler provides previewHandler) {
+		EpisodeDetailScreen(state = state)
 	}
 }
