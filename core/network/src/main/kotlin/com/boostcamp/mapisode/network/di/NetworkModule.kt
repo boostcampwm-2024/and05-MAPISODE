117c7a769ab92f652ed42865c518a7a2889b703a
Ameri-Kano <yeokkim13@naver.com>
2024-11-27 12:15:54 +0900

    refactor/#114 : Repository DI 분리

--- core/network/src/main/kotlin/com/boostcamp/mapisode/network/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/boostcamp/mapisode/network/di/NetworkModule.kt
@@ -1,7 +1,5 @@
 package com.boostcamp.mapisode.network.di
 
-import com.boostcamp.mapisode.network.repository.NaverMapsRepository
-import com.boostcamp.mapisode.network.repository.NaverMapsRepositoryImpl
 import com.boostcamp.mapisode.network.retrofit.NaverMapsApi
 import com.boostcamp.mapisode.network.retrofit.NaverMapsApiClient
 import dagger.Module
@@ -17,8 +15,4 @@ object NetworkModule {
 	@Provides
 	@Singleton
 	fun provideNaverMapsApi(): NaverMapsApi = NaverMapsApiClient.create()
-
-	@Provides
-	fun bindNaverMapsRepository(naverMapsApi: NaverMapsApi): NaverMapsRepository =
-		NaverMapsRepositoryImpl(naverMapsApi)
 }
