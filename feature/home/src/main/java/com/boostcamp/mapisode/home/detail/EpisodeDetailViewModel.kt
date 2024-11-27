6b9b0f8bb6064a1ad597f891f307834db1a91402
HaeTi <t9h9@naver.com>
2024-11-28 01:54:05 +0900

    feat/#125 : 에피소드 상세에서 전달받은 에피소드 id로 정보 조회

--- feature/home/src/main/java/com/boostcamp/mapisode/home/detail/EpisodeDetailViewModel.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/detail/EpisodeDetailViewModel.kt
@@ -1,11 +1,18 @@
 package com.boostcamp.mapisode.home.detail
 
+import androidx.lifecycle.viewModelScope
+import com.boostcamp.mapisode.episode.EpisodeRepository
+import com.boostcamp.mapisode.home.R
 import com.boostcamp.mapisode.ui.base.BaseViewModel
+import com.boostcamp.mapisode.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 internal class EpisodeDetailViewModel @Inject constructor(
+	private val episodeRepository: EpisodeRepository,
+	private val userRepository: UserRepository,
 ) : BaseViewModel<EpisodeDetailIntent, EpisodeDetailState, EpisodeDetailSideEffect>(
 	EpisodeDetailState(),
 ) {
@@ -18,6 +25,28 @@ internal class EpisodeDetailViewModel @Inject constructor(
 	}
 
 	private fun loadEpisodeDetail(episodeId: String) {
+		viewModelScope.launch {
+			intent { copy(isLoading = true) }
 
+			try {
+				val episode = episodeRepository.getEpisodeById(episodeId)
+				if (episode != null) {
+					val author = userRepository.getUserInfo(episode.createdBy)
+					intent {
+						copy(
+							isLoading = false,
+							episode = episode,
+							author = author,
+						)
+					}
+				} else {
+					intent { copy(isLoading = false) }
+					postSideEffect(EpisodeDetailSideEffect.ShowToast(R.string.episode_detail_not_found_error))
+				}
+			} catch (e: Exception) {
+				intent { copy(isLoading = false) }
+				postSideEffect(EpisodeDetailSideEffect.ShowToast(R.string.episode_detail_load_error))
+			}
+		}
 	}
 }
