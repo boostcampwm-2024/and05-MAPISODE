124c6c204ff7ea064ae0954876b654104c61be5e
TaewoongR <iwoong98@kau.kr>
2024-11-28 01:54:12 +0900

    fix/#136 : textField 내부 수정
    - 텍스트 필드 포커스 활성화 공간 수정 : 거의 모든 텍스트 필드 영역에서 포커스를 가져옴
    - isError 인자가 true일 때 배경과 텍스트에 색상 변경
    - 모든 인자 사용하도록 수정

--- core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/MapisodeTextField.kt
+++ core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/MapisodeTextField.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -64,7 +66,6 @@ fun MapisodeTextField(
 	keyboardOptions: KeyboardOptions = KeyboardOptions(
 		imeAction = ImeAction.Done,
 	),
-	keyboardActions: KeyboardActions = KeyboardActions.Default,
 	singleLine: Boolean = false,
 	maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
 	minLines: Int = 1,
@@ -75,7 +76,7 @@ fun MapisodeTextField(
 	cursorColor: Color = MapisodeTheme.colorScheme.textFieldStroke,
 	errorContainerColor: Color = MapisodeTheme.colorScheme.seeIconColor,
 	errorTextColor: Color = MapisodeTheme.colorScheme.seeIconColor,
-	strokeColor: Color = MapisodeTheme.colorScheme.textFieldStroke,
+	strokeColor: Color = if (isError) errorTextColor else MapisodeTheme.colorScheme.textFieldStroke,
 	strokeWidth: Dp = 1.dp,
 ) {
 	@Suppress("NAME_SHADOWING")
@@ -83,7 +84,7 @@ fun MapisodeTextField(
 	val focusManager = LocalFocusManager.current
 	var isFocused by remember { mutableStateOf(false) }
 
-	Box(
+	Column(
 		modifier = modifier
 			.width(320.dp)
 			.height(42.dp)
@@ -91,77 +92,96 @@ fun MapisodeTextField(
 				minWidth = 320.dp,
 				minHeight = 42.dp,
 			)
-			.clickable(
-				interactionSource = interactionSource,
-				indication = null,
-				enabled = enabled && !readOnly,
-			) { }
 			.onFocusChanged { focusState ->
 				if (isFocused && !focusState.isFocused) {
 					onSubmitInput(value)
 				}
 				isFocused = focusState.isFocused
-			}
-			.border(strokeWidth, strokeColor, shape)
-			.background(if (isError) errorContainerColor else containerColor, shape)
-			.padding(vertical = 12.dp, horizontal = 16.dp),
+			},
 	) {
-		Row(
-			modifier = Modifier.fillMaxWidth(),
-			verticalAlignment = Alignment.CenterVertically,
-			horizontalArrangement = Arrangement.SpaceBetween,
-		) {
-			if (leadingIcon != null) {
-				leadingIcon()
+		if (label != null) {
+			Box(modifier = Modifier.padding(bottom = 4.dp)) {
+				label()
 			}
-			BasicTextField(
-				value = value,
-				onValueChange = onValueChange,
-				modifier = Modifier
-					.weight(1f)
-					.fillMaxWidth(),
-				enabled = enabled,
-				readOnly = readOnly,
-				textStyle = textStyle.copy(color = textColor).toTextStyle(),
-				cursorBrush = SolidColor(cursorColor),
-				visualTransformation = visualTransformation,
-				keyboardOptions = keyboardOptions,
-				keyboardActions = KeyboardActions(
-					onDone = {
-						focusManager.clearFocus()
+		}
+		Box(
+			modifier = Modifier
+				.clickable(
+					interactionSource = interactionSource,
+					indication = null,
+					enabled = enabled && !readOnly,
+				) { }
+				.border(strokeWidth, strokeColor, shape)
+				.background(
+					if (isError) {
+						errorContainerColor.copy(alpha = 0.1f)
+					} else {
+						containerColor
 					},
-				),
-				interactionSource = interactionSource,
-				singleLine = singleLine,
-				maxLines = maxLines,
-				minLines = minLines,
-				decorationBox = { innerTextField ->
-					Box(
-						modifier = Modifier.fillMaxWidth(),
-						contentAlignment = Alignment.CenterStart,
-					) {
-						if (prefix != null) {
-							Box(modifier = Modifier.padding(end = 8.dp)) {
-								prefix()
+					shape,
+				)
+				.padding(vertical = 12.dp, horizontal = 16.dp),
+		) {
+			Row(
+				verticalAlignment = Alignment.CenterVertically,
+				horizontalArrangement = Arrangement.SpaceBetween,
+			) {
+				if (leadingIcon != null) {
+					leadingIcon()
+				}
+				BasicTextField(
+					value = value,
+					onValueChange = onValueChange,
+					modifier = Modifier
+						.fillMaxSize()
+						.weight(0.8f),
+					enabled = enabled,
+					readOnly = readOnly,
+					textStyle = textStyle.copy(
+						color = if (isError) {
+							errorTextColor
+						} else {
+							textColor
+						},
+					).toTextStyle(),
+					cursorBrush = SolidColor(cursorColor),
+					visualTransformation = visualTransformation,
+					keyboardOptions = keyboardOptions,
+					keyboardActions = KeyboardActions(
+						onDone = { focusManager.clearFocus() },
+					),
+					interactionSource = interactionSource,
+					singleLine = singleLine,
+					maxLines = maxLines,
+					minLines = minLines,
+					decorationBox = { innerTextField ->
+						Box(
+							modifier = Modifier.fillMaxWidth(),
+							contentAlignment = Alignment.CenterStart,
+						) {
+							if (prefix != null) {
+								Box(modifier = Modifier.padding(end = 8.dp)) {
+									prefix()
+								}
+							}
+
+							if (value.isEmpty() && placeholder.isNotBlank()) {
+								MapisodePlaceHolder(value = placeholder)
+							}
+
+							innerTextField()
+
+							if (suffix != null) {
+								Box(modifier = Modifier.padding(start = 8.dp)) {
+									suffix()
+								}
 							}
 						}
-
-						if (value.isEmpty() && placeholder.isNotBlank()) {
-							MapisodePlaceHolder(value = placeholder)
-						}
-
-						innerTextField()
-
-						if (suffix != null) {
-							Box(modifier = Modifier.padding(start = 8.dp)) {
-								suffix()
-							}
-						}
-					}
-				},
-			)
-			if (trailingIcon != null) {
-				trailingIcon()
+					},
+				)
+				if (trailingIcon != null) {
+					trailingIcon()
+				}
 			}
 		}
 	}
