69ca552df137c1d2ee7bb524d89ac0efe2a415c5
shinythinking <shinythink@gmail.com>
2024-11-28 12:59:01 +0900

    feat/#138 : AuthState에 로딩 상태 추가

--- feature/login/src/main/java/com/boostcamp/mapisode/login/AuthState.kt
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/AuthState.kt
@@ -4,6 +4,7 @@ import com.boostcamp.mapisode.model.AuthData
 import com.boostcamp.mapisode.ui.base.UiState
 
 data class AuthState(
+	val isLoading: Boolean = true,
 	val isLoginSuccess: Boolean = false,
 	val authData: AuthData? = null,
 	val nickname: String = "",
