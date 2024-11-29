bf357a9c0a532cf5b8327242f15daaa4d9b99e3a
shinythinking <shinythink@gmail.com>
2024-11-29 20:36:01 +0900

    fix/#134 : 권한을 한번에 하나만 물어보는 오류 해결

---
+++ core/ui/src/main/kotlin/com/boostcamp/mapisode/ui/photopicker/permissionHandler/PermissionHandler.kt
@@ -0,0 +1,44 @@
+package com.boostcamp.mapisode.ui.photopicker.permissionHandler
+
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun PermissionHandler(
+	isCameraNeeded: Boolean,
+	onPermissionsGranted: () -> Unit,
+	onPermissionsDenied: () -> Unit,
+) {
+	val context = LocalContext.current
+	var hasPermissions by remember {
+		mutableStateOf(PermissionCheck(context).hasPermissions(isCameraNeeded))
+	}
+
+	val launcher = rememberLauncherForActivityResult(
+		contract = ActivityResultContracts.RequestMultiplePermissions(),
+	) { permissions ->
+		val allGranted = permissions.values.all { it }
+		hasPermissions = allGranted
+
+		if (allGranted) {
+			onPermissionsGranted()
+		} else {
+			onPermissionsDenied()
+		}
+	}
+
+	LaunchedEffect(hasPermissions) {
+		if (!hasPermissions) {
+			launcher.launch(PermissionCheck.getRequiredPermissions(isCameraNeeded))
+		} else {
+			onPermissionsGranted()
+		}
+	}
+}
