c8035c55eb541bf0bbde33269078b7499a248abc
shinythinking <shinythink@gmail.com>
2024-11-29 21:43:39 +0900

    feat/#134 : photoInfo 클래스 parcelable로 변경

--- core/ui/src/main/kotlin/com/boostcamp/mapisode/ui/photopicker/model/PhotoInfo.kt
+++ core/ui/src/main/kotlin/com/boostcamp/mapisode/ui/photopicker/model/PhotoInfo.kt
@@ -1,8 +1,11 @@
 package com.boostcamp.mapisode.ui.photopicker.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.Date
 
+@Parcelize
 data class PhotoInfo(
 	val uri: String,
 	val dateTaken: Date?,
-)
+) : Parcelable
