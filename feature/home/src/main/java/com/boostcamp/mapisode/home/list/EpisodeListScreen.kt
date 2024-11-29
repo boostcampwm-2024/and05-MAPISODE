cf10f06b7bc34f5370bcb31ab90643ce8361e3df
HaeTi <t9h9@naver.com>
2024-11-29 19:05:50 +0900

    feat/#128 : 그룹 내 에피소드 불러오기

--- feature/home/src/main/java/com/boostcamp/mapisode/home/list/EpisodeListScreen.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/list/EpisodeListScreen.kt
@@ -1,10 +1,58 @@
 package com.boostcamp.mapisode.home.list
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.boostcamp.mapisode.designsystem.compose.MapisodeCircularLoadingIndicator
 
 @Composable
 fun EpisodeListRoute(
 	groupId: String,
+	viewModel: EpisodeListViewModel = hiltViewModel(),
+	onBackClick: () -> Unit,
+) {
+	val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+	val context = LocalContext.current
+
+	LaunchedEffect(Unit) {
+		viewModel.onIntent(EpisodeListIntent.LoadEpisodeList(groupId))
+	}
+
+	LaunchedEffect(Unit) {
+		viewModel.sideEffect.collect { sideEffect ->
+			when (sideEffect) {
+				is EpisodeListSideEffect.ShowToast -> {
+					val message = context.getString(sideEffect.messageResId)
+					Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+				}
+			}
+		}
+	}
+
+	if (uiState.isLoading) {
+		Box(
+			modifier = Modifier.fillMaxSize(),
+			contentAlignment = Alignment.Center,
+		) {
+			MapisodeCircularLoadingIndicator()
+		}
+	} else {
+		EpisodeListScreen(state = uiState, onBackClick = onBackClick)
+	}
+}
+
+@Composable
+fun EpisodeListScreen(
+	state: EpisodeListState,
+	onBackClick: () -> Unit,
 ) {
 
 }
--- feature/home/src/main/java/com/boostcamp/mapisode/home/list/EpisodeListSideEffect.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/list/EpisodeListSideEffect.kt
@@ -1,1 +1,7 @@
 package com.boostcamp.mapisode.home.list
+
+import com.boostcamp.mapisode.ui.base.SideEffect
+
+sealed class EpisodeListSideEffect : SideEffect {
+	data class ShowToast(val messageResId: Int) : EpisodeListSideEffect()
+}
