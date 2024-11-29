9784576d385049b00dabb24271514f7b3cdcc28e
shinythinking <shinythink@gmail.com>
2024-11-29 19:21:12 +0900

    feat/#134 : MapisodePhotoPicker 추가!
    1. isPhotoPickerClicked : 포토피커를 보여줄지 결정하는 상태 변수
    2. 분기문을 이용해서 photopicker 화면과 기존 화면 분리
    3. photopicker 사용!

--- feature/login/src/main/java/com/boostcamp/mapisode/login/SignUpScreen.kt
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/SignUpScreen.kt
@@ -1,5 +1,6 @@
 package com.boostcamp.mapisode.login
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,12 +11,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.boostcamp.mapisode.designsystem.R.drawable
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
@@ -23,88 +28,120 @@ import com.boostcamp.mapisode.designsystem.compose.MapisodeScaffold
 import com.boostcamp.mapisode.designsystem.compose.MapisodeText
 import com.boostcamp.mapisode.designsystem.compose.MapisodeTextField
 import com.boostcamp.mapisode.designsystem.compose.button.MapisodeFilledButton
-import com.boostcamp.mapisode.designsystem.compose.button.MapisodeOutlinedButton
+import com.boostcamp.mapisode.designsystem.compose.button.MapisodeImageButton
 import com.boostcamp.mapisode.designsystem.compose.topbar.TopAppBar
 import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
+import com.boostcamp.mapisode.ui.photopicker.MapisodePhotoPicker
 
 @Composable
 fun SignUpScreen(
 	nickname: String,
 	onNicknameChanged: (String) -> Unit,
+	profileUrl: String,
+	onProfileUrlchange: (String) -> Unit,
 	onSignUpClick: () -> Unit,
 	modifier: Modifier = Modifier,
 ) {
-	MapisodeScaffold(
-		modifier = modifier.fillMaxSize(),
-		isStatusBarPaddingExist = true,
-		isNavigationBarPaddingExist = true,
-		topBar = { SignUpTopBar() },
-	) { paddingValues ->
-		Box(
-			modifier = modifier
-				.padding(paddingValues)
-				.fillMaxSize(),
-			contentAlignment = Alignment.Center,
-		) {
-			Column(
-				modifier = Modifier
-					.fillMaxWidth(0.85f),
-				horizontalAlignment = Alignment.CenterHorizontally,
+	val isPhotoPickerClicked = rememberSaveable { mutableStateOf(false) }
+
+	if (isPhotoPickerClicked.value) {
+		MapisodePhotoPicker(
+			numOfPhoto = 1,
+			onPhotoSelected = { selectedPhotos ->
+				onProfileUrlchange(selectedPhotos[0].uri)
+				isPhotoPickerClicked.value = false
+			},
+			onBackPressed = { isPhotoPickerClicked.value = false },
+			isCameraNeeded = true,
+		)
+	} else {
+		MapisodeScaffold(
+			modifier = modifier.fillMaxSize(),
+			isStatusBarPaddingExist = true,
+			isNavigationBarPaddingExist = true,
+			topBar = { SignUpTopBar() },
+		) { paddingValues ->
+			Box(
+				modifier = modifier
+					.padding(paddingValues)
+					.fillMaxSize(),
+				contentAlignment = Alignment.Center,
 			) {
-				Spacer(modifier = Modifier.weight(0.5f))
-
-				MapisodeText(
-					text = stringResource(R.string.login_nickname),
-					modifier = Modifier.align(Alignment.Start),
-					style = MapisodeTheme.typography.titleLarge,
-				)
-
-				Spacer(modifier = Modifier.height(12.dp))
-
-				MapisodeTextField(
-					value = nickname,
-					onValueChange = onNicknameChanged,
-					placeholder = stringResource(R.string.login_nickname_placeholder),
+				Column(
 					modifier = Modifier
-						.fillMaxWidth(),
-					keyboardOptions = KeyboardOptions(
-						imeAction = ImeAction.Done,
-					),
-				)
+						.fillMaxWidth(0.85f),
+					horizontalAlignment = Alignment.CenterHorizontally,
+				) {
+					Spacer(modifier = Modifier.weight(0.5f))
 
-				Spacer(modifier = Modifier.weight(2f))
+					MapisodeText(
+						text = stringResource(R.string.login_nickname),
+						modifier = Modifier.align(Alignment.Start),
+						style = MapisodeTheme.typography.titleLarge,
+					)
 
-				MapisodeText(
-					text = stringResource(R.string.login_profile_image),
-					modifier = Modifier.align(Alignment.Start),
-					style = MapisodeTheme.typography.titleLarge,
-				)
+					Spacer(modifier = Modifier.height(12.dp))
 
-				Spacer(modifier = Modifier.height(12.dp))
+					MapisodeTextField(
+						value = nickname,
+						onValueChange = onNicknameChanged,
+						placeholder = stringResource(R.string.login_nickname_placeholder),
+						modifier = Modifier
+							.fillMaxWidth(),
+						keyboardOptions = KeyboardOptions(
+							imeAction = ImeAction.Done,
+						),
+					)
 
-				MapisodeOutlinedButton(
-					text = "placeholder for 이미지를 업로드해주세요",
-					onClick = { },
-					modifier = Modifier
-						.fillMaxWidth()
-						.aspectRatio(1f),
-				)
+					Spacer(modifier = Modifier.weight(2f))
 
-				Spacer(modifier = Modifier.weight(3f))
+					MapisodeText(
+						text = stringResource(R.string.login_profile_image),
+						modifier = Modifier.align(Alignment.Start),
+						style = MapisodeTheme.typography.titleLarge,
+					)
 
-				MapisodeFilledButton(
-					text = stringResource(R.string.login_next),
-					onClick = onSignUpClick,
-					modifier = Modifier
-						.weight(1f)
-						.fillMaxWidth(),
-				)
+					Spacer(modifier = Modifier.height(12.dp))
 
-				Spacer(modifier = Modifier.weight(0.6f))
+					MapisodeImageButton(
+						onClick = { isPhotoPickerClicked.value = true },
+						showImage = profileUrl.isBlank(),
+						modifier = Modifier
+							.fillMaxWidth()
+							.aspectRatio(1f),
+					) {
+						Column(
+							modifier = Modifier.fillMaxSize(),
+							horizontalAlignment = Alignment.CenterHorizontally,
+							verticalArrangement = Arrangement.Center,
+						) {
+							AsyncImage(
+								model = profileUrl,
+								contentDescription = "프로필 이미지",
+								modifier = Modifier
+									.fillMaxSize(),
+								contentScale = ContentScale.Crop,
+							)
+						}
+					}
+
+					Spacer(modifier = Modifier.weight(3f))
+
+					MapisodeFilledButton(
+						text = stringResource(R.string.login_next),
+						onClick = onSignUpClick,
+						modifier = Modifier
+							.weight(1f)
+							.fillMaxWidth(),
+					)
+
+					Spacer(modifier = Modifier.weight(0.6f))
+				}
 			}
 		}
 	}
 }
+
 
 @Composable
 fun SignUpTopBar() {
@@ -130,9 +167,11 @@ fun SignUpTopBar() {
 fun SignUpScreenPreview() {
 	MapisodeTheme {
 		SignUpScreen(
-			nickname = "nickname",
-			onNicknameChanged = { },
-			onSignUpClick = { },
+			nickname = "닉네임",
+			onNicknameChanged = {},
+			profileUrl = "",
+			onProfileUrlchange = {},
+			onSignUpClick = {},
 		)
 	}
 }
