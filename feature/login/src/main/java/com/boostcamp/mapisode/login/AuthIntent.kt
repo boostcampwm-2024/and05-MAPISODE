47bb60973044eac62e16145c33cc430295d36a5d
shinythinking <shinythink@gmail.com>
2024-11-29 19:21:45 +0900

    feat/#134 : profileUrl 변경 intent 추가

--- feature/login/src/main/java/com/boostcamp/mapisode/login/AuthIntent.kt
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/AuthIntent.kt
@@ -6,6 +6,7 @@ import com.boostcamp.mapisode.ui.base.UiIntent
 sealed interface AuthIntent : UiIntent {
 	data class OnGoogleSignInClick(val googleOauth: GoogleOauth) : AuthIntent
 	data class OnNicknameChange(val nickname: String) : AuthIntent
+	data class OnProfileUrlchange(val profileUrl: String) : AuthIntent
 	data object OnSignUpClick : AuthIntent
 	data object OnAutoLogin : AuthIntent
 	data object OnLoginSuccess : AuthIntent
