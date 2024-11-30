9fea72145bfdf68137a76f16f423296b391c234c
HaeTi <t9h9@naver.com>
2024-12-01 03:12:40 +0900

    feat/#128 : 정렬 타입에 따른 리스트 반환

--- feature/home/src/main/java/com/boostcamp/mapisode/home/list/EpisodeListViewModel.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/list/EpisodeListViewModel.kt
@@ -3,8 +3,11 @@ package com.boostcamp.mapisode.home.list
 import androidx.lifecycle.viewModelScope
 import com.boostcamp.mapisode.episode.EpisodeRepository
 import com.boostcamp.mapisode.home.R
+import com.boostcamp.mapisode.home.common.SortOption
+import com.boostcamp.mapisode.model.EpisodeModel
 import com.boostcamp.mapisode.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,6 +18,7 @@ class EpisodeListViewModel @Inject constructor(private val episodeRepository: Ep
 	override fun onIntent(intent: EpisodeListIntent) {
 		when (intent) {
 			is EpisodeListIntent.LoadEpisodeList -> loadEpisodeList(intent.groupId)
+			is EpisodeListIntent.ChangeSortOption -> changeSortOption(intent.sortOption)
 		}
 	}
 
@@ -27,5 +31,28 @@ class EpisodeListViewModel @Inject constructor(private val episodeRepository: Ep
 				postSideEffect(EpisodeListSideEffect.ShowToast(R.string.episode_detail_load_error))
 			}
 		}
+	}
+
+	private fun changeSortOption(sortOption: SortOption) {
+		val sortedEpisodes = sortEpisodes(currentState.episodes, sortOption)
+		intent {
+			copy(
+				selectedSortOption = sortOption,
+				episodes = sortedEpisodes,
+			)
+		}
+	}
+
+	private fun sortEpisodes(
+		episodes: List<EpisodeModel>,
+		sortOption: SortOption,
+	): PersistentList<EpisodeModel> {
+		val sortedList = when (sortOption) {
+			SortOption.LATEST -> episodes.sortedByDescending { it.createdAt }
+			SortOption.OLDEST -> episodes.sortedBy { it.createdAt }
+			SortOption.NAME -> episodes.sortedBy { it.title }
+		}
+
+		return sortedList.toPersistentList()
 	}
 }
