ab03d673356aae2d41b197b46355767615a295f5
TaewoongR <iwoong98@kau.kr>
2024-12-02 04:26:17 +0900

    fix/#174 : coil 이미지 contentScale 조정
    - 규격 bound에 모든 사진 영역이 가득 차도록 설정

--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/screen/GroupEditScreen.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/screen/GroupEditScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
@@ -68,7 +69,7 @@ fun GroupEditScreen(
 	).value
 
 	BackHandler {
-		if (uiState.value.isSelectingGroupImage ) {
+		if (uiState.value.isSelectingGroupImage) {
 			viewModel.onIntent(GroupEditIntent.OnBackToGroupCreation)
 		} else {
 			viewModel.onIntent(GroupEditIntent.OnBackClick)
@@ -230,6 +231,7 @@ fun GroupEditField(
 							model = profileUrl,
 							contentDescription = "그룹 이미지",
 							modifier = Modifier.fillMaxSize(),
+							contentScale = ContentScale.FillBounds,
 						)
 					}
 				}
