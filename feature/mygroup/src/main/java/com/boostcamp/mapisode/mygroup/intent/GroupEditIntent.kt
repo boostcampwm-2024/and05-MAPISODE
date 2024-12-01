b4c4fc16108410cc5bb805a218446fd3aefe0acf
TaewoongR <iwoong98@kau.kr>
2024-12-02 03:35:29 +0900

    fix/#174 : 그룹 편집 포토피커 적용

--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/intent/GroupEditIntent.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/intent/GroupEditIntent.kt
@@ -5,8 +5,12 @@ import com.boostcamp.mapisode.ui.base.UiIntent
 
 @Immutable
 sealed class GroupEditIntent : UiIntent {
-	data class LoadGroups(val groupId: String) : GroupEditIntent()
+	data class Initialize(val groupId: String) : GroupEditIntent()
 	data object OnBackClick : GroupEditIntent()
 	data class OnGroupEditClick(val title: String, val content: String, val imageUrl: String) :
 		GroupEditIntent()
+	data object OnPhotoPickerClick : GroupEditIntent()
+	data class OnGroupImageSelect(val imageUrl: String) : GroupEditIntent()
+	data object OnBackToGroupCreation : GroupEditIntent()
+	data object DenyPhotoPermission : GroupEditIntent()
 }
