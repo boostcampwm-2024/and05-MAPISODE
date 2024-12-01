276336477c54249b6812dced36f497570061bd79
Ameri-Kano <yeokkim13@naver.com>
2024-12-01 23:41:29 +0900

    refactor/#163 : 사용하지 않는 상수 삭제

--- feature/episode/src/main/java/com/boostcamp/mapisode/episode/common/NewEpisodeConstant.kt
+++ feature/episode/src/main/java/com/boostcamp/mapisode/episode/common/NewEpisodeConstant.kt
@@ -18,16 +18,4 @@ object NewEpisodeConstant {
 		.fillMaxWidth()
 		.fillMaxHeight(0.3f)
 	val textFieldVerticalArrangement = Arrangement.spacedBy(12.dp)
-
-	private const val CATEGORY_NAME_EAT = "먹을 것"
-	private const val CATEGORY_NAME_SEE = "볼 것"
-	private const val CATEGORY_NAME_OTHER = "나머지"
-	private const val CATEGORY_VALUE_EAT = "EAT"
-	private const val CATEGORY_VALUE_SEE = "SEE"
-	private const val CATEGORY_VALUE_OTHER = "OTHER"
-	val categoryMap = mapOf(
-		CATEGORY_NAME_EAT to CATEGORY_VALUE_EAT,
-		CATEGORY_NAME_SEE to CATEGORY_VALUE_SEE,
-		CATEGORY_NAME_OTHER to CATEGORY_VALUE_OTHER,
-	)
 }
