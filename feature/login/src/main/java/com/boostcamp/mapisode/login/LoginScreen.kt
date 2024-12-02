b6b5499479b9b5ce754951d8fbb343356b07f917
shinythinking <shinythink@gmail.com>
2024-12-02 23:20:42 +0900

    ui/#191 : 구글 소셜 로그인 버튼

--- feature/login/src/main/java/com/boostcamp/mapisode/login/LoginScreen.kt
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/LoginScreen.kt
@@ -1,29 +1,23 @@
 package com.boostcamp.mapisode.login
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.boostcamp.mapisode.designsystem.R.drawable
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
-import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
 import com.boostcamp.mapisode.designsystem.compose.MapisodeText
 import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
 
@@ -64,50 +58,32 @@ fun LoginScreen(
 			style = MapisodeTheme.typography.headlineSmall,
 		)
 
-		Spacer(modifier = Modifier.height(15.dp))
+		Spacer(modifier = Modifier.height(16.dp))
 
 		MapisodeText(
-			text = stringResource(R.string.login_app_long_description),
+			text = stringResource(R.string.login_app_long_description1),
 			modifier = Modifier.align(Alignment.CenterHorizontally),
-			style = MapisodeTheme.typography.bodyLarge.copy(
-				textAlign = TextAlign.Center,
-			),
+			style = MapisodeTheme.typography.bodyLarge,
+		)
+
+		Spacer(modifier = Modifier.height(4.dp))
+
+		MapisodeText(
+			text = stringResource(R.string.login_app_long_description2),
+			modifier = Modifier.align(Alignment.CenterHorizontally),
+			style = MapisodeTheme.typography.bodyLarge,
 		)
 
 		Spacer(modifier = Modifier.weight(1f))
 
-		MapisodeIconButton(
-			onClick = googleSignInClicked,
+		Image(
+			painter = painterResource(drawable.ic_google_login),
+			contentDescription = stringResource(R.string.login_google_login),
 			modifier = Modifier
-				.fillMaxWidth(0.8f)
-				.weight(1f)
-				.border(
-					width = 1.dp,
-					color = MapisodeTheme.colorScheme.outlineButtonStroke,
-				),
-		) {
-			Row(
-				modifier = Modifier.fillMaxSize(),
-				verticalAlignment = Alignment.CenterVertically,
-				horizontalArrangement = Arrangement.Center,
-			) {
-				Image(
-					painter = painterResource(id = drawable.ic_google),
-					contentDescription = "구글 로그인",
-					modifier = Modifier
-						.fillMaxHeight(0.5f)
-						.aspectRatio(1f),
-					contentScale = ContentScale.Fit,
-				)
-
-				Spacer(modifier = Modifier.width(24.dp))
-
-				MapisodeText(
-					text = "구글 계정으로 로그인",
-					style = MapisodeTheme.typography.bodyLarge,
-				)
-			}
-		}
+				.fillMaxWidth(0.5f)
+				.clickable { googleSignInClicked() },
+			contentScale = ContentScale.FillWidth,
+		)
 
 		Spacer(modifier = Modifier.weight(2f))
 	}
