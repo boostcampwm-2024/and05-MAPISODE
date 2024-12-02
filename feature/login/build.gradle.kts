2920bbbb83702a5c1f51bfb44cd36223dbb386de
shinythinking <shinythink@gmail.com>
2024-12-03 00:34:49 +0900

    chore/#196 : 의존성 설정

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -10,5 +10,6 @@ dependencies {
 	implementation(projects.core.auth)
 	implementation(projects.domain.user)
 	implementation(projects.domain.mygroup)
+	implementation(projects.domain.storage)
 	implementation(libs.bundles.coil)
 }
