33594a86883795ae7e40cc5089132a931f372d4b
shinythinking <shinythink@gmail.com>
2024-12-02 12:13:53 +0900

    fix/#160 : ktlint 대응 3

--- core/auth/src/main/java/com/boostcamp/mapisode/auth/GoogleOauth.kt
+++ core/auth/src/main/java/com/boostcamp/mapisode/auth/GoogleOauth.kt
@@ -98,16 +98,11 @@ class GoogleOauth(private val context: Context) {
 		GetSignInWithGoogleOption.Builder(BuildConfig.GOOGLE_WEB_CLIENT_ID)
 			.setNonce(generateNonce()).build()
 
-	private fun createCredentialRequest(googleIdOption: GetSignInWithGoogleOption):
-		GetCredentialRequest = GetCredentialRequest
-		.Builder()
-		.addCredentialOption(googleIdOption).build()
+	private fun createCredentialRequest(googleIdOption: GetSignInWithGoogleOption): GetCredentialRequest =
+		GetCredentialRequest.Builder().addCredentialOption(googleIdOption).build()
 
 	private fun validateCredential(credential: Credential): GoogleIdTokenCredential {
-		if (credential is CustomCredential
-			&& credential.type
-			== GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
-		) {
+		if (credential is CustomCredential && credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
 			return GoogleIdTokenCredential.createFrom(credential.data)
 		} else {
 			throw RuntimeException("유효하지 않는 credential type")
