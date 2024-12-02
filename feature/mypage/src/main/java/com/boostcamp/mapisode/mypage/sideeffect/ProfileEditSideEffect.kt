f9cc49db5b2ac68ea7973a29b5f5b3fdbe69698c
shinythinking <shinythink@gmail.com>
2024-12-02 22:29:33 +0900

    refactor/#160 : 불필요한 sideEffect 삭제

--- feature/mypage/src/main/java/com/boostcamp/mapisode/mypage/sideeffect/ProfileEditSideEffect.kt
+++ feature/mypage/src/main/java/com/boostcamp/mapisode/mypage/sideeffect/ProfileEditSideEffect.kt
@@ -3,7 +3,6 @@ package com.boostcamp.mapisode.mypage.sideeffect
 import com.boostcamp.mapisode.ui.base.SideEffect
 
 sealed interface ProfileEditSideEffect : SideEffect {
-	data object Idle : ProfileEditSideEffect
 	data class ShowToast(val messageResId: Int) : ProfileEditSideEffect
 	data object NavigateToMypage : ProfileEditSideEffect
 }
