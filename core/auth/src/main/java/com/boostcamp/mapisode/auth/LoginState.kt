56efe8e2f3d49ba2ac45c939acf60c5b64357408
shinythinking <shinythink@gmail.com>
2024-12-02 20:57:20 +0900

    fix/#192 : 사용자가 로그인을 취소하면 토스트를 띄우지 않도록 수정

--- core/auth/src/main/java/com/boostcamp/mapisode/auth/LoginState.kt
+++ core/auth/src/main/java/com/boostcamp/mapisode/auth/LoginState.kt
@@ -5,4 +5,5 @@ import com.boostcamp.mapisode.model.AuthData
 sealed interface LoginState {
 	data class Success(val idToken: String, val authDataInfo: AuthData) : LoginState
 	data class Error(val message: String) : LoginState
+	data object Cancel : LoginState
 }
