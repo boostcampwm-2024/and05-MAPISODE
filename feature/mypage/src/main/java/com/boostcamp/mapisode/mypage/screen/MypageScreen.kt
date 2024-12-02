5e57297fbaf29a3d5ff47b53ff8f8dc8f827f0c1
shinythinking <shinythink@gmail.com>
2024-12-02 11:51:26 +0900

    feat/#160 : MypageScreen 구현

--- feature/mypage/src/main/java/com/boostcamp/mapisode/mypage/screen/MypageScreen.kt
+++ feature/mypage/src/main/java/com/boostcamp/mapisode/mypage/screen/MypageScreen.kt
@@ -1,5 +1,6 @@
 package com.boostcamp.mapisode.mypage.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,17 +16,23 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
+import com.boostcamp.mapisode.auth.GoogleOauth
 import com.boostcamp.mapisode.designsystem.R.drawable.ic_arrow_forward_ios
 import com.boostcamp.mapisode.designsystem.R.drawable.ic_document
 import com.boostcamp.mapisode.designsystem.R.drawable.ic_edit
-import com.boostcamp.mapisode.designsystem.R.drawable.ic_withdrawal
 import com.boostcamp.mapisode.designsystem.compose.IconSize
+import com.boostcamp.mapisode.designsystem.compose.MapisodeDialog
 import com.boostcamp.mapisode.designsystem.compose.MapisodeDivider
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIcon
 import com.boostcamp.mapisode.designsystem.compose.MapisodeIconButton
@@ -34,16 +41,78 @@ import com.boostcamp.mapisode.designsystem.compose.MapisodeText
 import com.boostcamp.mapisode.designsystem.compose.Thickness
 import com.boostcamp.mapisode.designsystem.compose.topbar.TopAppBar
 import com.boostcamp.mapisode.designsystem.theme.MapisodeTheme
+import com.boostcamp.mapisode.mypage.MypageIntent
+import com.boostcamp.mapisode.mypage.MypageSideEffect
+import com.boostcamp.mapisode.mypage.MypageViewModel
 import com.boostcamp.mapisode.mypage.R
 
 @Composable
 internal fun MypageRoute(
 	onProfileEditClick: () -> Unit,
+	onNavigateToLogin: () -> Unit,
+	viewModel: MypageViewModel = hiltViewModel(),
 ) {
+	val context = LocalContext.current
+	val googleOauth = GoogleOauth(context)
+	val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+	LaunchedEffect(Unit) {
+		viewModel.onIntent(MypageIntent.Init)
+	}
+
+	LaunchedEffect(Unit) {
+		viewModel.sideEffect.collect { sideEffect ->
+			when (sideEffect) {
+				is MypageSideEffect.Idle -> {}
+				is MypageSideEffect.NavigateToLoginScreen -> onNavigateToLogin()
+				is MypageSideEffect.NavigateToEditScreen -> onProfileEditClick()
+				is MypageSideEffect.ShowToast -> {
+					Toast.makeText(
+						context,
+						sideEffect.messageResId,
+						Toast.LENGTH_SHORT,
+					).show()
+				}
+
+			}
+		}
+	}
+
+	if (uiState.showWithdrawalDialog) {
+		MapisodeDialog(
+			titleText = context.getString(R.string.mypage_withdrawal_title),
+			contentText = context.getString(R.string.mypage_withdrawal_message),
+			confirmText = context.getString(R.string.mypage_withdrawal_confirm),
+			cancelText = "취소",
+			onResultRequest = { isConfirm ->
+				when (isConfirm) {
+					true -> {
+						viewModel.onIntent(MypageIntent.ConfirmClick(googleOauth))
+					}
+
+					false -> {}
+				}
+			},
+			onDismissRequest = {
+				viewModel.onIntent(MypageIntent.TurnOffDialog)
+			},
+		)
+	}
+
 	MypageScreen(
-		name = "이름",
-		profileUrl = "https://avatars.githubusercontent.com/u/77449569?v=4",
+		name = uiState.name,
+		profileUrl = uiState.profileUrl,
+		onLogoutClick = { viewModel.onIntent(MypageIntent.LogoutClick(googleOauth)) },
 		onProfileEditClick = onProfileEditClick,
+		onPrivacyPolicyClick = {
+			viewModel.onIntent(
+				MypageIntent.PrivacyPolicyClick(
+					context,
+					true,
+				),
+			)
+		},
+		onWithdrawalClick = { viewModel.onIntent(MypageIntent.WithdrawalClick) },
 	)
 }
 
@@ -51,7 +120,10 @@ internal fun MypageRoute(
 private fun MypageScreen(
 	name: String,
 	profileUrl: String,
+	onLogoutClick: () -> Unit,
 	onProfileEditClick: () -> Unit,
+	onPrivacyPolicyClick: () -> Unit,
+	onWithdrawalClick: () -> Unit,
 	modifier: Modifier = Modifier,
 ) {
 	MapisodeScaffold(
@@ -65,7 +137,7 @@ private fun MypageScreen(
 				) {
 					MapisodeText(
 						text = stringResource(R.string.mypage_logout),
-						modifier = Modifier.clickable { },
+						modifier = Modifier.clickable { onLogoutClick() },
 						style = MapisodeTheme.typography.labelLarge,
 					)
 				}
@@ -111,16 +183,16 @@ private fun MypageScreen(
 				MyPageContent(
 					text = stringResource(R.string.mypage_terms_of_use),
 					iconId = ic_document,
-					onClick = { },
+					onClick = onPrivacyPolicyClick,
 				)
 
 				Spacer(modifier = Modifier.height(20.dp))
 
-				MyPageContent(
-					text = stringResource(R.string.mypage_withdrawal),
-					iconId = ic_withdrawal,
-					onClick = { },
-				)
+//				MyPageContent(
+//					text = stringResource(R.string.mypage_withdrawal),
+//					iconId = ic_withdrawal,
+//					onClick = {},
+//				)
 			}
 		}
 	}
@@ -142,7 +214,7 @@ private fun Profile(
 			model = profileUrl,
 			contentDescription = stringResource(R.string.mypage_profile_image),
 			modifier = Modifier
-				.size(48.dp)
+				.size(52.dp)
 				.clip(CircleShape),
 		)
 
@@ -150,7 +222,7 @@ private fun Profile(
 
 		MapisodeText(
 			text = name,
-			style = MapisodeTheme.typography.bodyLarge,
+			style = MapisodeTheme.typography.titleLarge,
 		)
 
 		Spacer(modifier = Modifier.weight(1f))
