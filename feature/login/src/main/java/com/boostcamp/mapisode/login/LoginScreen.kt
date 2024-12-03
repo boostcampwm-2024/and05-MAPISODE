f1aaf580168d19f2be701e580567da9133d44d19
shinythinking <shinythink@gmail.com>
2024-12-03 13:54:04 +0900

    feat/#191 : 구글 버튼 디자인 수정

--- feature/login/src/main/java/com/boostcamp/mapisode/login/LoginScreen.kt
+++ feature/login/src/main/java/com/boostcamp/mapisode/login/LoginScreen.kt
@@ -1,13 +1,22 @@
 package com.boostcamp.mapisode.login
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -76,14 +85,40 @@ fun LoginScreen(
 
 		Spacer(modifier = Modifier.weight(1f))
 
-		Image(
-			painter = painterResource(drawable.ic_google_login),
-			contentDescription = stringResource(R.string.login_google_login),
+		Row(
 			modifier = Modifier
-				.fillMaxWidth(0.5f)
+				.height(56.dp)
+				.fillMaxWidth(0.7f)
+				.background(
+					color = MapisodeTheme.colorScheme.outlineButtonBackground,
+					shape = RoundedCornerShape(4.dp),
+				)
+				.border(
+					width = 1.dp,
+					color = MapisodeTheme.colorScheme.outlineButtonStroke,
+					shape = RoundedCornerShape(4.dp),
+				)
 				.clickable { googleSignInClicked() },
-			contentScale = ContentScale.FillWidth,
-		)
+			verticalAlignment = Alignment.CenterVertically,
+			horizontalArrangement = Arrangement.Center,
+		) {
+			Image(
+				painter = painterResource(drawable.ic_google),
+				contentDescription = stringResource(R.string.login_google_login),
+				modifier = Modifier
+					.padding(start = 12.dp, end = 10.dp)
+					.size(28.dp),
+				contentScale = ContentScale.Fit,
+			)
+
+			MapisodeText(
+				text = stringResource(R.string.login_google_button_text),
+				modifier = Modifier.wrapContentWidth(),
+				style = MapisodeTheme.typography.bodyLarge,
+			)
+
+			Spacer(modifier = Modifier.width(12.dp))
+		}
 
 		Spacer(modifier = Modifier.weight(2f))
 	}
