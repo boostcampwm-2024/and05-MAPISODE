8d2f467e274a104c3ef2c6e43030f2106a0d3cbb
TaewoongR <iwoong98@kau.kr>
2024-12-03 21:44:14 +0900

    refactor/#202 : fillBounds -> crop 이미지 표시

--- core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/card/GroupInfoCard.kt
+++ core/designsystem/src/main/kotlin/com/boostcamp/mapisode/designsystem/compose/card/GroupInfoCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -44,6 +45,7 @@ fun GroupInfoCard(
 			modifier = Modifier
 				.size(140.dp)
 				.clip(shape = RoundedCornerShape(16.dp)),
+			contentScale = ContentScale.Crop,
 		)
 		MapisodeDivider(thickness = Thickness.Thick)
 		Column(
