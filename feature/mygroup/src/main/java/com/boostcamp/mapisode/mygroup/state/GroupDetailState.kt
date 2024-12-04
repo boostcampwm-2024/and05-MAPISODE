4ec647acc337f89cbac1f45c6f01eafdc0c096bd
TaewoongR <iwoong98@kau.kr>
2024-12-04 14:08:31 +0900

    fix/#220 : immutable state 로 변경
    - core:model의 모델을 state로 사용 -> 화면 전용 immutable 모델 선언 및 state 사용

--- feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/state/GroupDetailState.kt
+++ feature/mygroup/src/main/java/com/boostcamp/mapisode/mygroup/state/GroupDetailState.kt
@@ -1,17 +1,19 @@
 package com.boostcamp.mapisode.mygroup.state
 
 import androidx.compose.runtime.Immutable
-import com.boostcamp.mapisode.model.GroupModel
 import com.boostcamp.mapisode.mygroup.model.GroupUiEpisodeModel
 import com.boostcamp.mapisode.mygroup.model.GroupUiMemberModel
+import com.boostcamp.mapisode.mygroup.model.GroupUiModel
 import com.boostcamp.mapisode.ui.base.UiState
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 @Immutable
 data class GroupDetailState(
 	val isGroupIdCaching: Boolean = true,
 	val isGroupLoading: Boolean = false,
 	val isGroupOwner: Boolean = false,
-	val group: GroupModel? = null,
-	val membersInfo: List<GroupUiMemberModel> = emptyList(),
-	val episodes: List<GroupUiEpisodeModel> = emptyList(),
+	val group: GroupUiModel? = null,
+	val membersInfo: ImmutableList<GroupUiMemberModel> = persistentListOf(),
+	val episodes: ImmutableList<GroupUiEpisodeModel> = persistentListOf(),
 ) : UiState
