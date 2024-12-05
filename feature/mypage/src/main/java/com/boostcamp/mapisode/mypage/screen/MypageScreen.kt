cd720ad541ff46c5a6441a7c367d9ac7cb31d8c0
shinythinking <shinythink@gmail.com>
2024-12-05 18:13:30 +0900

    refactor/#225 : 마이페이지 프로필 Crop으로 변경

--- feature/mypage/src/main/java/com/boostcamp/mapisode/mypage/screen/MypageScreen.kt
+++ feature/mypage/src/main/java/com/boostcamp/mapisode/mypage/screen/MypageScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -220,6 +221,7 @@ private fun Profile(
 			modifier = Modifier
 				.size(52.dp)
 				.clip(CircleShape),
+			contentScale = ContentScale.Crop,
 		)
 
 		Spacer(modifier = Modifier.width(16.dp))
